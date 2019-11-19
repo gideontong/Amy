56cfa6ccd7c66b62dbf26f9a2fccb0a649f18dc3
Gideon Tong <gideon@gideontong.com>
2019-11-18 22:31:46 -0800

    Kind of recognizes text

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
+mic = sr.Microphone()
 
+"""
 record = sr.AudioFile('audio.wav')
 with record as source:
     audio = r.record(source)
+"""
+
+with mic as source:
+    audio = r.listen(source)
 
 text = r.recognize_google(audio)
+print(text)

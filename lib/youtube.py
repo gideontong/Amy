735cd655e1be1a2168a7ff4d783e8578f3a60c3a
Gideon Tong <gideon@gideontong.com>
2019-11-19 23:37:34 -0800

    YouTube playback

--- lib/youtube.py
+++ lib/youtube.py
@@ -1,6 +1,21 @@
 from youtube_dl import YoutubeDL
+import urllib.request
+import urllib.parse
+import re
+import vlc
 
-uri = ''
-downloadOptions = {}
+query_string = urllib.parse.urlencode({"search_query" : input()})
+html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+uri = "http://www.youtube.com/watch?v=" + search_results[0]
+
+print(uri)
+downloadOptions = {
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '128'
+    }]
+}
 with YoutubeDL(downloadOptions) as ydl:
     ydl.download([uri])

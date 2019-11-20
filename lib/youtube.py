3351fe85fff42e01a1b771a8ef84a1cf0a9a7791
Gideon Tong <gideon@gideontong.com>
2019-11-19 23:39:32 -0800

    Get ready for streaming method

--- lib/youtube.py
+++ lib/youtube.py
@@ -3,6 +3,7 @@ import urllib.request
 import urllib.parse
 import re
 import vlc
+import pafy
 
 query_string = urllib.parse.urlencode({"search_query" : input()})
 html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
@@ -10,6 +11,8 @@ search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().de
 uri = "http://www.youtube.com/watch?v=" + search_results[0]
 
 print(uri)
+
+"""
 downloadOptions = {
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -19,3 +22,4 @@ downloadOptions = {
 }
 with YoutubeDL(downloadOptions) as ydl:
     ydl.download([uri])
+"""

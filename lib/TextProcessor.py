38d38686b413a7e5bd111d14b495cef1b8d1e3c4
Gideon Tong <gideon@gideontong.com>
2019-12-15 10:41:28 -0800

    This took way to long to figure out

--- lib/TextProcessor.py
+++ lib/TextProcessor.py
@@ -1,4 +1,2 @@
 def main():
     print("hi")
-
-main()
--- lib/youtube.py
+++ lib/youtube.py
@@ -3,8 +3,31 @@ import urllib.request
 import urllib.parse
 import re
 import pafy
+import vlc
 
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+video = pafy.new(url)
+print("got url")
+best = video.getbest()
+print("got best")
+playurl = best.url
+print("url should be", playurl)
 
+Instance = vlc.Instance()
+print("started vlc instance")
+player = Instance.media_player_new()
+Media = Instance.media_new(playurl)
+print("here's an instance")
+Media.get_mrl()
+player.set_media(Media)
+print("set media")
+player.play()
+print("should've played")
+
+while True:
+    pass
+
+"""
 query_string = urllib.parse.urlencode({"search_query" : input()})
 html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
@@ -17,6 +40,7 @@ video = pafy.new(uri)
 bestaudio = video.getbestaudio()
 bestaudio.download()
 #playurl = best.url
+"""
 
 """
 downloadOptions = {

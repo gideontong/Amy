3dfe2dd4117bc74a7c3e8742cd534200b70eda29
Gideon Tong <gideon@gideontong.com>
2019-12-15 16:28:53 -0800

    youtube-dl no longer necessary

--- lib/VideoStream.py
+++ lib/VideoStream.py
@@ -1,28 +1,33 @@
+import datetime
 import pafy
 import vlc
 
 url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 video = pafy.new(url)
 print("got url")
+length = video.duration
+print("video is", length, "seconds long")
 best = video.getbest()
-print("got best")
+# print("got best")
 playurl = best.url
-print("url should be", playurl)
+# print("url should be", playurl)
 
 Instance = vlc.Instance()
 print("started vlc instance")
 player = Instance.media_player_new()
 Media = Instance.media_new(playurl)
-print("here's an instance")
+# print("here's an instance")
 Media.get_mrl()
 player.set_media(Media)
-print("set media")
+# print("set media")
 player.play()
 print("should've played")
 
 # to do
 # cloes the window after done
 # hide the vide
+
+
 
 while True:
     pass

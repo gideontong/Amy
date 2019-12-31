5bbe02e377ca8fa7210d60282fe1369729b0bda7
Gideon Tong <gideon@gideontong.com>
2019-12-30 19:07:44 -0800

    Moved into function

--- lib/VideoStream.py
+++ lib/VideoStream.py
@@ -2,26 +2,27 @@ from datetime import datetime
 import pafy
 import vlc
 
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-video = pafy.new(url)
-print("got url")
-length = video.duration
-print("video is", length, "seconds long")
-best = video.getbest()
-# print("got best")
-playurl = best.url
-# print("url should be", playurl)
+def test():
+    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    video = pafy.new(url)
+    print("got url")
+    length = video.duration
+    print("video is", length, "seconds long")
+    best = video.getbest()
+    # print("got best")
+    playurl = best.url
+    # print("url should be", playurl)
 
-Instance = vlc.Instance()
-print("started vlc instance")
-player = Instance.media_player_new()
-Media = Instance.media_new(playurl)
-# print("here's an instance")
-Media.get_mrl()
-player.set_media(Media)
-# print("set media")
-player.play()
-print("should've played")
-player.stop()
+    Instance = vlc.Instance()
+    print("started vlc instance")
+    player = Instance.media_player_new()
+    Media = Instance.media_new(playurl)
+    # print("here's an instance")
+    Media.get_mrl()
+    player.set_media(Media)
+    # print("set media")
+    player.play()
+    print("should've played")
+    player.stop()
 
 # Still need to hide the video

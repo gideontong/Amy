6129175cde187e4568e8d7e6f1816d72b0834bd3
Gideon Tong <gideon@gideontong.com>
2019-12-30 19:15:11 -0800

    Run as --exert to work

--- main.py
+++ main.py
@@ -11,6 +11,27 @@ def versionCheck():
         sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
 def main():
+    argumentList = sys.argv[1:]
+    unixOptions = "hvte"
+    gnuOptions = ["help", "verbose", "test", "exert"]
+
+    try:
+        arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+    except getopt.error as err:
+        print(str(err))
+        sys.exit(2)
+
+    for argument, value in arguments:
+        if argument in ("-v", "--verbose"):
+            print("--verbose was run! Nothing was done!")
+        if argument in ("-t", "--test"):
+            print("--test was run! Nothing was done!")
+        if argument in ("-e", "--exert"):
+            global MIN_PYTHON
+            MIN_PYTHON = (MIN_PYTHON[0], 8)
+        else:
+            print("Nothing was run! Nothing was done!")
+
     versionCheck()
 
     r = sr.Recognizer()

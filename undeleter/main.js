3e1ac1358f8bb2d80486ba012061104ff84e8b89
Gideon Tong <gideon@gideontong.com>
2020-06-21 16:09:49 -0700

    Added a silent easter egg

--- undeleter/main.js
+++ undeleter/main.js
@@ -33,4 +33,17 @@ client.on('messageDelete', message => {
     message.reply(responses[Math.floor(Math.random() * responses.length)]);
 });
 
+client.on('message', message => {
+    if(message.content.toLowerCase().includes('valorant')) {
+        if(Math.random() < 0.5) {
+            if(Math.random() < 0.9) {
+                message.reply("it's time to play Valorant!");
+            } else {
+                message.reply("it's not time to play Valorant...");
+            }
+        }
+        console.log(`${message.author} mentioned Valorant at ${new Date()}`);
+    }
+})
+
 client.login(secrets.token);

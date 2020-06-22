4fe7b9ee9a96205d8bdc2a0314f23d83c742cf73
Gideon Tong <gideon@gideontong.com>
2020-06-21 21:13:24 -0700

    Improve Max's SkyFactory code

--- events/message.js
+++ events/message.js
@@ -4,7 +4,8 @@ const log = require('log4js').getLogger('amy');
 
 module.exports = async message => {
     if(message.author.bot) return;
-    if(message.content.toLowerCase().includes('valorant')) {
+    sanitizedMessage = message.content.toLowerCase();
+    if(sanitizedMessage.includes('valorant')) {
         if(Math.random() < 0.5) {
             if(Math.random() < 0.9) {
                 message.reply("it's time to play Valorant!");
@@ -13,8 +14,7 @@ module.exports = async message => {
             }
         }
         log.info(`${message.author} mentioned VALORANT!`)
-    }
-    if(message.content.toLowerCase().includes('skyfactory')) {
+    } else if(sanitizedMessage.includes('skyfactory')) {
         if(Math.random() < 1.0) {
             if(Math.random() < 0.9) {
                 message.reply("it's time to play Sky Factory 4!");
@@ -22,6 +22,6 @@ module.exports = async message => {
                 message.reply("it's not time to play Sky Factory 4...");
             }
         }
-        console.log(`${message.author} mentioned SkyFactory at ${new Date()}`);
+        log.info(`${message.author} mentioned SkyFactory!`);
     }
 }

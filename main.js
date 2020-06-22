ac031542f2821a9a0d61e2fbd80c25bcedc53228
Gideon Tong <gideon@gideontong.com>
2020-06-21 20:05:06 -0700

    Separate responses into config file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-const secrets = require("./secrets.json");
+const secrets = require("./config/secrets.json");
+const responses = require("./config/responses.json");
 
 client.on("ready", () => {
     client.user.setActivity('Leo Zhang', { type: 'WATCHING' })
@@ -10,27 +10,11 @@ client.on("ready", () => {
     console.log("Online");
 });
 
-responses = [
-    " decided to delete a message. Naughty!",
-    " is hiding something again...",
-    " is sneaky, but I am sneakier.",
-    " is making me a little nervous!",
-    " is so fast, even Mank Demer is put to shame.",
-    " is so fat, they just ate a whole pizza.",
-    " tried to kiss me, but I am bethrothed to Leo.",
-    " made me cry.",
-    " is gaslighting me.",
-    " tried to convince me that I'm stupid.",
-    " uses 0% of their brain power.",
-    " is such a kindhearted soul, but didn't think first.",
-    " is hiding something in their rice cooker.",
-    " got too drunk last night."
-]
+kek = responses.delete;
 
 client.on('messageDelete', message => {
     console.log(`Deleted ${new Date()}: ${message.author} said "${message.cleanContent}"`);
-    // client.channels.get("CHANNEL_ID").send(`A message saying "${message.cleanContent}" has been deleted at ${new Date()}`)
-    message.reply(responses[Math.floor(Math.random() * responses.length)]);
+    message.reply(kek[Math.floor(Math.random() * kek.length)]);
 });
 
 client.on('message', message => {

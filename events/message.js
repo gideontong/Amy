c17b34292d3156b9f08d72d2575c53f8a129438c
Gideon Tong <gideon@gideontong.com>
2020-06-25 12:32:19 -0700

    Fixed message and logging bugs

--- events/message.js
+++ events/message.js
@@ -10,12 +10,12 @@ module.exports = async message => {
         commands = message.content.split(" ");
         replyIndex = message.content.indexOf(';');
         if (replyIndex < 0) return;
-        reply = message.content.substring(replyIndex);
+        reply = message.content.substring(replyIndex + 1);
         if (isNaN(commands[1]) || isNaN(commands[2])) return;
         const channel = message.client.channels.cache.get(commands[1]);
         message.delete();
         channel.send('<@' + commands[2] + '> ' + reply);
-        log.info(`${message.athor.tag} ${message.author} triggered a manual bot message!`);
+        log.info(`${message.author.tag} ${message.author} triggered a manual bot message!`);
     }
     sanitizedMessage = message.content.toLowerCase();
     if (sanitizedMessage.includes('valorant')) {

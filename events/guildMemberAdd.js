94263cb6f8b520de6871937aab915938406401e3
Gideon Tong <gideon@gideontong.com>
2020-06-26 00:45:29 -0700

    Removed punctuation from logging

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,5 +4,5 @@ const log = require('log4js').getLogger('amy');
 // Handler for a new member added
 module.exports = async member => {
     if (member.user.bot) return;
-    log.info(`${member.tag} ${member} joined the server!`);
+    log.info(`${member.tag} ${member} joined the server`);
 }
--- events/message.js
+++ events/message.js
@@ -20,7 +20,7 @@ module.exports = async message => {
                 message.channel.send("Wiped recent history.")
                     .then(msg => msg.delete(timeout));
             });
-            log.info(`${message.author.tag} ${message.author} deleted ${count} messages in ${message.channel}.`);
+            log.info(`${message.author.tag} ${message.author} deleted ${count} messages in ${message.channel}`);
         } else {
             replyIndex = message.content.indexOf(';');
             if (replyIndex < 0) return;
@@ -29,7 +29,7 @@ module.exports = async message => {
             const channel = message.client.channels.cache.get(commands[1]);
             message.delete();
             channel.send('<@' + commands[2] + '> ' + reply);
-            log.info(`${message.author.tag} ${message.author} triggered a manual bot message!`);
+            log.info(`${message.author.tag} ${message.author} triggered a manual bot message that said ${reply}`);
         }
     }
     sanitizedMessage = message.content.toLowerCase();
@@ -41,29 +41,29 @@ module.exports = async message => {
                 message.reply("it's not time to play Valorant...");
             }
         }
-        log.info(`${message.author.tag} ${message.author} mentioned VALORANT!`)
+        log.info(`${message.author.tag} ${message.author} mentioned VALORANT`)
     } else if (sanitizedMessage.includes('skyfactory')) {
         if (Math.random() < 0.9) {
             message.reply("it's time to play Sky Factory 4!");
         } else {
             message.reply("it's not time to play Sky Factory 4...");
         }
-        log.info(`${message.author.tag} ${message.author} mentioned SkyFactory!`);
+        log.info(`${message.author.tag} ${message.author} mentioned SkyFactory`);
     } else if (sanitizedMessage.includes(' of leo')) {
         response = responses.requester[Math.floor(Math.random() * responses.requester.length)]
             + ": " + responses.photos[Math.floor(Math.random() * responses.photos.length)];
         message.reply(response);
-        log.info(`${message.author.tag} ${message.author} requested a picture of Leo!`);
+        log.info(`${message.author.tag} ${message.author} requested a picture of Leo`);
     } else if (message.author.id == targets.leo) {
         if (Math.random() < 0.2) {
             response = responses.prank[Math.floor(Math.random() * responses.prank.length)];
             message.reply(response);
-            log.info(`${message.author.tag} ${message.author} triggered a prank response!`);
+            log.info(`${message.author.tag} ${message.author} triggered a prank response`);
         }
     } else if (sanitizedMessage.substr(0, 3) == "amy" || sanitizedMessage.includes(message.client.user.id)) {
         if (Math.random() < 0.05) {
             message.reply("you called me?");
         }
-        log.info(`${message.author.tag} ${message.author} mentioned me...`)
+        log.info(`${message.author.tag} ${message.author} mentioned me`)
     }
 }

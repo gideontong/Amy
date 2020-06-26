29fc3199d16d1a14253466f0e11907b23ca54b3e
Gideon Tong <gideon@gideontong.com>
2020-06-26 13:21:00 -0700

    Made changing probabilities an option

--- commands/qr.js
+++ commands/qr.js
@@ -1,6 +1,7 @@
 // Imports from local config files
 const config = require('../config/config.json');
 const strings = config.strings.qr;
+const constants = config.constants;
 
 // Imports from dependencies
 const { MessageAttachment } = require('discord.js');
@@ -20,7 +21,7 @@ module.exports = async (bot, msg, args) => {
         log.info(`${msg.author.tag} ${msg.author} tried to make a big QR code`);
         return;
     }
-    if (Math.random() < 0.01) text = strings.troll;
+    if (Math.random() < constants.trollProbability) text = strings.troll;
     QRCode.toDataURL(text, function (err, url) {
         const buffer = new Buffer(url, 'base64');
         const image = new MessageAttachment(buffer);

a610ebb9e39e6de8a9bca55e10facc9cb149ac21
Gideon Tong <gideon@gideontong.com>
2020-06-26 13:20:19 -0700

    Updated config strings for ease of use

--- commands/qr.js
+++ commands/qr.js
@@ -1,6 +1,6 @@
 // Imports from local config files
 const config = require('../config/config.json');
-const strings = config.strings;
+const strings = config.strings.qr;
 
 // Imports from dependencies
 const { MessageAttachment } = require('discord.js');
@@ -10,17 +10,17 @@ const log = require('log4js').getLogger('amy');
 // Handler for running qr command
 module.exports = async (bot, msg, args) => {
     if (args < 2) {
-        msg.reply(strings.qr.empty);
+        msg.reply(strings.empty);
         log.info(`${msg.author.tag} ${msg.author} tried to make an empty QR code`);
         return;
     }
     var text = msg.content.substring(args[0].length + 1);
     if (text.length > 512) {
-        msg.reply(strings.qr.tooLarge);
+        msg.reply(strings.tooLarge);
         log.info(`${msg.author.tag} ${msg.author} tried to make a big QR code`);
         return;
     }
-    if (Math.random() < 0.01) text = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    if (Math.random() < 0.01) text = strings.troll;
     QRCode.toDataURL(text, function (err, url) {
         const buffer = new Buffer(url, 'base64');
         const image = new MessageAttachment(buffer);

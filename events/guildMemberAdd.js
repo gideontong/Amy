16bc75d2e20ff82d113fda2df08fc7e4adfa02a9
Gideon Tong <gideon@gideontong.com>
2020-06-26 00:58:09 -0700

    Update new member DM to use config

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,13 @@
+// Import from local config files
+const config = require('../config/config.json');
+const strings = config.strings;
+
 // Imports from dependencies
 const log = require('log4js').getLogger('amy');
 
 // Handler for a new member added
 module.exports = async member => {
     if (member.user.bot) return;
-    member.send(`Hey! Welcome to the Dudes of 708 Discord server. If you have questions about the server, ask anyone with the tag **Dudes of 708**. Questions about me, feel free to ask **Gideon#5433**. I don't respond to messages yet, but enjoy your stay!`);
+    member.send(strings.newMember);
     log.info(`${member.user.tag} ${member} joined the server`);
 }

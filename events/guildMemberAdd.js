16c036b41165841ef4775abdfe5ec4c0186abd7e
Gideon Tong <gideon@gideontong.com>
2020-06-26 00:47:01 -0700

    Added a server greeting

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,5 +4,6 @@ const log = require('log4js').getLogger('amy');
 // Handler for a new member added
 module.exports = async member => {
     if (member.user.bot) return;
+    member.send(`Hey! Welcome to the Dudes of 708 Discord server. If you have questions about the server, ask anyone with the tag **Dudes of 708**. Questions about me, feel free to ask **Gideon#5433**. I don't respond to messages yet, but enjoy your stay!`);
     log.info(`${member.tag} ${member} joined the server`);
 }

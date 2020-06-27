794a5bd997d60e6b0cda03b12f982b504666edbf
Gideon Tong <gideon@gideontong.com>
2020-06-27 10:45:27 -0700

    Support !joined for tagging

--- commands/joined.js
+++ commands/joined.js
@@ -4,11 +4,15 @@ const log = require('log4js').getLogger('amy')
 // new Date(ID ? (ID / 4194304) + 1420070400000 : 0).toLocaleString()
 // Handler for running joined command
 module.exports = async (bot, msg, args) => {
-    var id = parseInt(args[1]) ? (args.legnth > 1 && !isNaN(args[1])) : msg.author.id;
-    log.info(id);
+    var id = msg.author.id;
+    var requester = "you";
+    if (args.length > 1) {
+        id = (!isNaN(args[1])) ? parseInt(args[1]) : parseInt(msg.mentions.members.firstKey);
+        requester = "they";
+    }
     const joined = new Date(id ? (id / 4194304) + 1420070400000 : 0)
         .toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-    msg.reply(`you (or the person you're requesting) joined at ${joined}`);
+    msg.reply(`${requester} joined at ${joined}`);
     // msg.reply(`you joined at ${joined}. Use \`!howold\` to see how old your Discord account is!`);
-    log.info(`${msg.author.tag} ${msg.author} requested their Discord account creation time`);
+    log.info(`${msg.author.tag} ${msg.author} requested a Discord account creation time`);
 }

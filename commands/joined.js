853297b5ed46959bcb2b711ca95b288438c27df5
Gideon Tong <gideon@gideontong.com>
2020-06-27 10:38:15 -0700

    Make a better joined at

--- commands/joined.js
+++ commands/joined.js
@@ -4,9 +4,10 @@ const log = require('log4js').getLogger('amy')
 // new Date(ID ? (ID / 4194304) + 1420070400000 : 0).toLocaleString()
 // Handler for running joined command
 module.exports = async (bot, msg, args) => {
-    const joined = new Date(msg.author.id ? (msg.author.id / 4194304) + 1420070400000 : 0)
+    var id = parseInt(args[1]) ? (args.legnth > 1 && !isNaN(args[1])) : msg.author.id;
+    const joined = new Date(id ? (id / 4194304) + 1420070400000 : 0)
         .toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-    msg.reply(`you joined at ${joined}`);
+    msg.reply(`you (or the person you're requesting) joined at ${joined}`);
     // msg.reply(`you joined at ${joined}. Use \`!howold\` to see how old your Discord account is!`);
     log.info(`${msg.author.tag} ${msg.author} requested their Discord account creation time`);
 }

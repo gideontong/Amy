30c76ce57e910d5aa401a9eaad552e8a25792571
Gideon Tong <gideon@gideontong.com>
2020-06-27 10:18:56 -0700

    Replace websites with config

--- commands/github.js
+++ commands/github.js
@@ -1,8 +1,11 @@
+// Imports from local config files
+const config = require('../config/config.json');
+
 // Imports from dependencies
 const log = require('log4js').getLogger('amy');
 
 // Handler for running website command
 module.exports = async (bot, msg, args) => {
-    msg.channel.send(`https://www.github.com/gideontong/Amy`);
+    msg.channel.send(config.links.github);
     log.info(`${msg.author.tag} ${msg.author} requested the GitHub repo`);
 }

8f66d359e85a8f8b2da854a94fb7d9b66504cb69
Gideon Tong <gideon@gideontong.com>
2020-06-28 16:14:15 -0700

    Update accountage error message

--- commands/accountage.js
+++ commands/accountage.js
@@ -7,9 +7,13 @@ const log = require('log4js').getLogger('amy')
 module.exports = async (bot, msg, args) => {
     var id = msg.author.id;
     if (args.length > 1) {
-        id = (!isNaN(args[1])) ? parseInt(args[1]) : BigInt(args[1].substring(3, args[1].length - 1));
+        if (args.length > 2) {
+            msg.reply('run `!accountage (ID)` to get your account age!');
+            return;
+        }
+        id = (!isNaN(args[1])) ? args[1] : args[1].substring(3, args[1].length - 1);
     }
     const timeSince = timeSinceSnowflake(id);
-    msg.channel.send(`<@${id}>'s account is ${timeSince} old!`);
+    msg.channel.send(`<@${id}>'s account is about ${timeSince} old!`);
     log.info(`${msg.author.tag} ${msg.author} requested the account age of ${id}`); 
 }

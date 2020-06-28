ec147a651a9250193bca55435e9e10b086cc927d
Gideon Tong <gideon@gideontong.com>
2020-06-28 15:30:24 -0700

    Replace with substring

--- commands/accountage.js
+++ commands/accountage.js
@@ -7,9 +7,9 @@ const log = require('log4js').getLogger('amy')
 module.exports = async (bot, msg, args) => {
     var id = msg.author.id;
     if (args.length > 1) {
-        id = (!isNaN(args[1])) ? parseInt(args[1]) : parseInt(msg.mentions.users.array()[0].id);
+        id = (!isNaN(args[1])) ? parseInt(args[1]) : parseInt(args[0].substring(2, args[0].length - 1));
     }
     const timeSince = timeSinceSnowflake(id);
-    msg.channel.send(`<@${id}>'s account is ${timeSince} old! ${msg.mentions.users.array()[0].tag}`);
+    msg.channel.send(`<@${id}>'s account is ${timeSince} old!`);
     log.info(`${msg.author.tag} ${msg.author} requested the account age of ${id}`); 
 }

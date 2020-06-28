45f7ee9f0ed2cfd943424c200bb037a8f98d6c51
Gideon Tong <gideon@gideontong.com>
2020-06-28 15:45:36 -0700

    Trying to fix more and more thiings

--- commands/accountage.js
+++ commands/accountage.js
@@ -7,7 +7,7 @@ const log = require('log4js').getLogger('amy')
 module.exports = async (bot, msg, args) => {
     var id = msg.author.id;
     if (args.length > 1) {
-        id = (!isNaN(args[1])) ? parseInt(args[1]) : BigInt(args[0].substring(2, args[0].length - 1));
+        id = (!isNaN(args[1])) ? parseInt(args[1]) : BigInt(args[0].substring(3, args[0].length - 1));
     }
     const timeSince = timeSinceSnowflake(id);
     msg.channel.send(`<@${id}>'s account is ${timeSince} old!`);

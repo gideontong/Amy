6ef4f1158cc9b31ba51ccf487fbc85f42a279535
Gideon Tong <gideon@gideontong.com>
2020-06-28 15:28:45 -0700

    first() -> array()

--- commands/accountage.js
+++ commands/accountage.js
@@ -7,9 +7,9 @@ const log = require('log4js').getLogger('amy')
 module.exports = async (bot, msg, args) => {
     var id = msg.author.id;
     if (args.length > 1) {
-        id = (!isNaN(args[1])) ? parseInt(args[1]) : parseInt(msg.mentions.users.first().id);
+        id = (!isNaN(args[1])) ? parseInt(args[1]) : parseInt(msg.mentions.users.array()[0].id);
     }
     const timeSince = timeSinceSnowflake(id);
-    msg.channel.send(`<@${id}>'s account is ${timeSince} old! ${msg.mentions.users.first().tag}`);
+    msg.channel.send(`<@${id}>'s account is ${timeSince} old! ${msg.mentions.users.array()[0].tag}`);
     log.info(`${msg.author.tag} ${msg.author} requested the account age of ${id}`); 
 }

583d11ac4d8a4b70e2e47d6bfb5bfba2f64679c8
Gideon Tong <gideon@gideontong.com>
2020-06-29 10:16:37 -0700

    Added a welcome message

---
+++ commands/welcome.js
@@ -0,0 +1,13 @@
+// Imports from local config files
+const config = require('../config/config.json');
+
+// Imports from dependencies
+const log = require('log4js').getLogger('amy');
+
+// Handler for running clear command
+module.exports = async (bot, msg, args) => {
+    if (!msg.author.id == targets.gideon) return;
+    msg.channel.send(config.welcome);
+    msg.channel.send('Oh, and one more thing. Here\'s the permanent invite link to this server: https://discord.gg/WUGMTcZ');
+    log.info(`${msg.author.tag} ${msg.author} triggered the welcome message in ${msg.channel}`);
+}

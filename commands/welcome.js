3e9e290a875f3c61c99c2a229854fceb32468384
Gideon Tong <gideon@gideontong.com>
2020-06-29 10:19:50 -0700

    Define targets

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,5 +1,6 @@
 // Imports from local config files
 const config = require('../config/config.json');
+const targets = config.targets;
 
 // Imports from dependencies
 const log = require('log4js').getLogger('amy');

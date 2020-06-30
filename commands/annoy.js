ed8b2f436d822d7f9ac028f5e6c6bb64a1e885f1
Gideon Tong <gideon@gideontong.com>
2020-06-30 00:38:59 -0700

    Preemptive start up annoy command

--- commands/annoy.js
+++ commands/annoy.js
@@ -1,2 +1,16 @@
+// Imports from dependencies
+const log = require('log4js').getLogger('amy');
+
+// Handler for running annoy command
 module.exports = async (bot, msg, args) => {
+    var id = msg.author.id;
+    if (args.length < 2) {
+        msg.reply('imagine not trying to annoy someone else? HAHAHA');
+    } else if (args.length > 2) {
+        msg.reply('run `!annoy (user)` to annoy them!');
+    } else {
+        var annoy = (!isNaN(args[1])) ? args[1] : args[1].substring(3, args[1].length - 1);
+        // Add something here to annoy the target user
+        log.info(`${msg.author.tag} ${msg.author} decided to annoy <@${annoy}>`);
+    }
 }

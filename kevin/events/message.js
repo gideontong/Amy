043361519842eda5b49ac3aab4f86f237e2498d5
Gideon Tong <gideon@gideontong.com>
2020-06-29 23:55:28 -0700

    Add command support to Kevin

--- kevin/events/message.js
+++ kevin/events/message.js
@@ -9,8 +9,29 @@ const log = require('log4js').getLogger('kevin');
 
 // Handler for a sent message
 module.exports = async message => {
-    if (message.author.bot || message.author == targets.kevin || message.content[0] == "!") return;
-    if (Math.random() < 0.01) {
+    if (message.author.bot || message.content[0] == "!") return;
+    if (messague.author == targets.kevin || message.author == targets.gideon) {
+        if (message.content[0] == "?") {
+            commands = message.content.split(" ");
+            if (message.content[1] != " ") {
+                message.delete();
+                try {
+                    cmdFile = require(`../commands/${commands[0].slice(1)}.js`);
+                } catch {
+                    log.warn(`${message.author.tag} ${message.author} tried to run an invalid command`);
+                    return;
+                }
+                if (!cmdFile) {
+                    log.warn(`${message.author.tag} ${message.author} ran a command that doesn't exist`);
+                    return;
+                } else {
+                    cmdFile(message.client, message, commands).catch(err => {
+                        log.error(`${message.author.tag} ${message.author} ran ${message.content} that resulted in error ${err}`);
+                    })
+                }
+            }
+        }
+    } else if (Math.random() < 0.01) {
         message.reply(sarcasm[Math.floor(Math.random() * sarcasm.length)]);
         log.info(`${message.author.tag} ${message.author} triggered a sarcastic response`);
     }

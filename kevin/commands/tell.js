9eb114aee72af20ed8e2e3481dbb95c99b8f44cf
Gideon Tong <gideon@gideontong.com>
2020-06-29 23:33:15 -0700

    Added tell command to Kevin

--- kevin/commands/tell.js
+++ kevin/commands/tell.js
@@ -1,14 +1,14 @@
 // Imports from local config files
-const config = require('../config/config.json');
+const config = require('../../config/config.json');
 const targets = config.targets;
 const strings = config.strings;
 
 // Imports from dependencies
-const log = require('log4js').getLogger('amy');
+const log = require('log4js').getLogger('kevin');
 
 // Handler for running tell command
 module.exports = async (bot, msg, args) => {
-    if (msg.author != targets.gideon || msg.author != targets.amy) {
+    if (msg.author != targets.gideon || msg.author != targets.kevin) {
         msg.reply(strings.notAdmin);
         log.info(`${msg.author.tag} ${msg.author} tried to force me to say something`);
     } else {

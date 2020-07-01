c25b40b980c834c2c747a6c37070afe0557b30c3
Gideon Tong <gideon@gideontong.com>
2020-06-30 17:07:27 -0700

    Add validChannel and extractSnowflake to Validation class

--- kevin/commands/tell.js
+++ kevin/commands/tell.js
@@ -11,6 +11,8 @@ module.exports = async (bot, msg, args) => {
     if (msg.author != targets.gideon && msg.author != targets.kevin) {
         msg.reply(strings.notAdmin);
         log.info(`${msg.author.tag} ${msg.author} tried to force me to say something`);
+    } else if (args.length > 2) {
+        //
     } else {
         msg.channel.send(msg.content.substring(args[0].length + 1));
         log.info(`${msg.author.tag} ${msg.author} told me to send a message`);

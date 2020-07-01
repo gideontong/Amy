215b393ed06332af92c5d5f7129b87d8353ef34f
Gideon Tong <gideon@gideontong.com>
2020-06-30 17:17:46 -0700

    ?tell should also now support channels

--- kevin/commands/tell.js
+++ kevin/commands/tell.js
@@ -1,5 +1,6 @@
 // Imports from local config files
 const config = require('../../config/config.json');
+const { extractSnowflake } = require('../../lib/Validation');
 const targets = config.targets;
 const strings = config.strings;
 
@@ -11,10 +12,17 @@ module.exports = async (bot, msg, args) => {
     if (msg.author != targets.gideon && msg.author != targets.kevin) {
         msg.reply(strings.notAdmin);
         log.info(`${msg.author.tag} ${msg.author} tried to force me to say something`);
+        return;
     } else if (args.length > 2) {
-        //
-    } else {
-        msg.channel.send(msg.content.substring(args[0].length + 1));
-        log.info(`${msg.author.tag} ${msg.author} told me to send a message`);
+        const snowflake = extractSnowflake(args[1]);
+        // More input validation may be needed
+        if (snowflake) {
+            const channel = msg.client.channels.cache.get(snowflake);
+            channel.send(message.content.substring(args[0].length + args[1].length + 2));
+            log.info(`${msg.author.tag} ${msg.author} told me to send a message in channel <#${snowflake}>`);
+            return;
+        }
     }
+    msg.channel.send(msg.content.substring(args[0].length + 1));
+    log.info(`${msg.author.tag} ${msg.author} told me to send a message`);
 }

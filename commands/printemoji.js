1d07d1df889c65747bc6a1d8f2f718764640a2ca
Gideon Tong <gideon@gideontong.com>
2020-07-01 16:16:24 -0700

    Fix typo in targets

--- commands/printemoji.js
+++ commands/printemoji.js
@@ -7,7 +7,7 @@ const log = require('log4js').getLogger('amy');
 
 // Handler for running printemoji command
 module.exports = async (bot, msg, args) => {
-    if (msg.author != targets.gdeon && msg.author != targets.amy) return;
+    if (msg.author != targets.gideon && msg.author != targets.amy) return;
     msg.channel.send(`<:${args[1]}:${args[2]}>`);
     log.info(`${msg.author.tag} ${msg.author} told me to repeat ${args[1]}`);
 }

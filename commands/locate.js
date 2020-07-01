eb92ef7045d9924be869ffc7cc61e72688a6f561
Gideon Tong <gideon@gideontong.com>
2020-07-01 10:13:54 -0700

    Add !youtube command

--- commands/locate.js
+++ commands/locate.js
@@ -1,2 +1,6 @@
+// Imports from dependencies
+const log = require('log4js').getLogger('amy');
+
+// Handler for running locate command
 module.exports = async (bot, msg, args) => {
 }
--- commands/youtube.js
+++ commands/youtube.js
@@ -1,2 +1,11 @@
+// Imports from local config files
+const links = require('../config/config.json').links;
+
+// Imports from dependencies
+const log = require('log4js').getLogger('amy');
+
+// Handler for running YouTube command
 module.exports = async (bot, msg, args) => {
+    msg.channel.send(links.youtube);
+    log.info(`${msg.author.tag} ${msg.author} requested the YouTube channel`);
 }

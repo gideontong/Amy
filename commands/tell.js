3117c21ceadb9bf3fe2032ea2efbc383fa532cfb
Gideon Tong <gideon@gideontong.com>
2020-06-30 18:03:59 -0700

    Introduce kevin->amy feature parity

--- commands/tell.js
+++ commands/tell.js
@@ -1,5 +1,6 @@
 // Imports from local config files
 const config = require('../config/config.json');
+const { validChannel, extractSnowflake } = require('../lib/Validation');
 const targets = config.targets;
 const strings = config.strings;
 
@@ -11,6 +12,15 @@ module.exports = async (bot, msg, args) => {
     if (msg.author != targets.gideon && msg.author != targets.amy) {
         msg.reply(strings.notAdmin);
         log.info(`${msg.author.tag} ${msg.author} tried to force me to say something`);
+    } else if (args.length > 2 && validChannel(args[1])) {
+        const snowflake = extractSnowflake(args[1]);
+        // More input validation may be needed
+        if (snowflake) {
+            const channel = msg.client.channels.cache.get(snowflake);
+            channel.send(msg.content.substring(args[0].length + args[1].length + 2));
+            log.info(`${msg.author.tag} ${msg.author} told me to send a message in <#${snowflake}>`);
+            return;
+        }
     } else {
         msg.channel.send(msg.content.substring(args[0].length + 1));
         log.info(`${msg.author.tag} ${msg.author} told me to send a message`);
--- kevin/commands/tell.js
+++ kevin/commands/tell.js
@@ -19,7 +19,7 @@ module.exports = async (bot, msg, args) => {
         if (snowflake) {
             const channel = msg.client.channels.cache.get(snowflake);
             channel.send(msg.content.substring(args[0].length + args[1].length + 2));
-            log.info(`${msg.author.tag} ${msg.author} told me to send a message in channel <#${snowflake}>`);
+            log.info(`${msg.author.tag} ${msg.author} told me to send a message in <#${snowflake}>`);
             return;
         }
     }

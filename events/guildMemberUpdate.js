ac9be42291e65e871f0621adc4c8b681a6ccd89b
Gideon Tong <gideon@gideontong.com>
2020-07-02 16:23:22 -0700

    Added some logging

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -8,10 +8,11 @@ const log = require('log4js').getLogger('amy');
 
 // Handler for a member update
 module.exports = async (oldMember, newMember) => {
-    const fakeMessage = new Message();
-    fakeMessage.author = newMember;
-    fakeMessage.channel = newMember.guild.channels.resolve(targets.general);
     if(newMember.nickname.toLowerCase().includes('amy')) {
+        const fakeMessage = new Message();
+        fakeMessage.author = newMember;
+        fakeMessage.channel = newMember.guild.channels.resolve(targets.general);
+        log.info(`A new member changed to include Amy, checking with ${fakeMessage}`);
         require('../commands/grantachievement')(newMember.client, fakeMessage, ['becomeAmy']);
     }
 }

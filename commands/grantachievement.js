977f6df88847bc3ac08eec1e179b6282a2f21650
Gideon Tong <gideon@gideontong.com>
2020-07-02 12:03:17 -0700

    Add sudo as a testing trigger

--- commands/grantachievement.js
+++ commands/grantachievement.js
@@ -8,6 +8,7 @@ const log = require('log4js').getLogger('amy');
 module.exports = async (bot, msg, args) {
     const buffer = await grantAchievement(args[1]);
     if (buffer) {
+        log.info(`${msg.author.tag} ${msg.author} achieved achievement ${args[1]}`);
         const image = new MessageAttachment(buffer);
         msg.reply(`aren't you feeling cool today!`, image);
     }

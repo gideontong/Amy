ace3df5dda378b0bf903e31b6ad9b7243c2a23ef
Gideon Tong <gideon@gideontong.com>
2020-07-02 16:19:00 -0700

    Add a channel for replying to fakeMessage

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,3 +1,7 @@
+// Imports from local config files
+const config = require('../config/config.json');
+const targets = config.targets;
+
 // Imports from dependencies
 const { Message } = require('discord.js');
 const log = require('log4js').getLogger('amy');
@@ -6,6 +10,7 @@ const log = require('log4js').getLogger('amy');
 module.exports = async (oldMember, newMember) => {
     const fakeMessage = new Message();
     fakeMessage.author = newMember;
+    fakeMessage.channel = newMember.guild.resolve(targets.general);
     if(newMember.nickname.toLowerCase().includes('amy')) {
         require('../commands/grantachievement')(newMember.client, fakeMessage, ['becomeAmy']);
     }

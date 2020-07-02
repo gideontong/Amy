8ef9e6f58fab5763cdd5859da5d6e4d7919863e1
Gideon Tong <gideon@gideontong.com>
2020-07-02 16:26:00 -0700

    Attempt to force fakeMessage to resolve

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -9,9 +9,8 @@ const log = require('log4js').getLogger('amy');
 // Handler for a member update
 module.exports = async (oldMember, newMember) => {
     if(newMember.nickname.toLowerCase().includes('amy')) {
-        var fakeMessage = new Message();
+        var fakeMessage = new Message(newMember.client, '', newMember.guild.channels.resolve(targets.general));
         fakeMessage.author = newMember;
-        fakeMessage.channel = newMember.guild.channels.resolve(targets.general);
         log.info(`A new member changed to include Amy, checking with ${fakeMessage}`);
         require('../commands/grantachievement')(newMember.client, fakeMessage, ['becomeAmy']);
     }

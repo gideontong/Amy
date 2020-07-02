10c87161332b430cb1cbc5a97e02547edcba226d
Gideon Tong <gideon@gideontong.com>
2020-07-02 16:15:37 -0700

    Pass a fake message object

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,10 +1,12 @@
 // Imports from dependencies
+const { Message } = require('discord.js');
 const log = require('log4js').getLogger('amy');
 
 // Handler for a member update
 module.exports = async (oldMember, newMember) => {
-    log.info(`Change detected, ${oldMember} ${newMember}`);
+    const fakeMessage = new Message();
+    fakeMessage.author = newMember;
     if(newMember.nickname.toLowerCase().includes('amy')) {
-        require('../commands/grantachievement')(newMember.client, newMember.lastMessage, ['becomeAmy']);
+        require('../commands/grantachievement')(newMember.client, fakeMessage, ['becomeAmy']);
     }
 }

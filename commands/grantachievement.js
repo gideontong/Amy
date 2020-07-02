60d9ad95a8734b3d8c7d746956756bf86ea18053
Gideon Tong <gideon@gideontong.com>
2020-07-02 11:58:10 -0700

    Add list of achievements to file

---
+++ commands/grantachievement.js
@@ -0,0 +1,10 @@
+// Imports from local config files
+const achievements = require('../config/achievements.json');
+
+// Imports from dependencies
+const log = require('log4js').getLogger('amy');
+
+// Wrapper for Achievement.grantAchievement()
+module.exports = async (bot, msg, args) {
+    //
+}
---
+++ lib/scripts/listAchievements.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+"""
+Usage: listAchievements (achievements.json) (public/secret)
+
+Lists all achievements, if no arguments are given, for
+a file in the same directory. Otherwise uses the file
+provided. Lists all unless specified.
+"""
+
+import argparse, json
+
+parser = argparse.ArgumentParser(description='List achievements.')
+parser.add_argument('file', nargs='?', default='achievements.json', help='achievements file')
+parser.add_argument('--public', action='store_true')
+parser.add_argument('--secret', action='store_true')
+args = parser.parse_args()
+array = []
+public = args.public
+secret = args.secret
+if not (secret and public):
+    public = True
+    secret = True
+# If file doesn't exist, program will crash. Sucks.
+with open(args.file, 'r') as achiDoc:
+    data = json.load(achiDoc)
+    if public:
+        for item in data['public']:
+            array.append(item['id'])
+    if secret:
+        for item in data['secret']:
+            array.append(item['id'])
+print(array)

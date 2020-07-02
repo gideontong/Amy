795231bac22d50e6a1b15ce0f22f963e6366820c
Gideon Tong <gideon@gideontong.com>
2020-07-02 12:01:04 -0700

    Update grantachievement

--- commands/grantachievement.js
+++ commands/grantachievement.js
@@ -1,10 +1,14 @@
-// Imports from local config files
-const achievements = require('../config/achievements.json');
+const { grantAchievement } = require('../lib/Achievement');
+const { MessageAttachment } = require('discord.js');
 
 // Imports from dependencies
 const log = require('log4js').getLogger('amy');
 
 // Wrapper for Achievement.grantAchievement()
 module.exports = async (bot, msg, args) {
-    //
+    const buffer = await grantAchievement(args[1]);
+    if (buffer) {
+        const image = new MessageAttachment(buffer);
+        msg.reply(`aren't you feeling cool today!`, image);
+    }
 }

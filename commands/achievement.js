4d29bf7c6be411d523a71c33bbb1fc7c7ba0dac0
Gideon Tong <gideon@gideontong.com>
2020-07-02 12:42:41 -0700

    Add achievement command

---
+++ commands/achievement.js
@@ -0,0 +1,27 @@
+// Imports from dependencies
+const { getAchievementData } = require('../lib/Achievement');
+const log = require('log4js').getLogger('amy');
+
+module.exports = async (bot, msg, args) => {
+    const data = await getAchievementData(args[1]);
+    if (data) {
+        const embed = {
+            "content": "Want to use your achievement as an emoji? That feature is coming soon! Vote for it on the bugtracker to make it a higher priority.",
+            "embeds":
+            {
+                "title": `${data.name}`,
+                "description": `${data.description}`,
+                "color": 14366683,
+                "footer": {
+                    "text": "Achieved by [coming soon] people."
+                },
+                "thumbnail": {
+                    "url": `https://amyhelps.ml/images/achievements/${data.id}.png`
+                }
+            }
+        }
+        msg.channel.send(embed);
+    } else {
+        msg.reply(`That's not exactly an achievement...`);
+    }
+}

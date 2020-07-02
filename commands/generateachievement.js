09c9171f4fd1454af7baff5aec39bdf84b438aab
Gideon Tong <gideon@gideontong.com>
2020-07-02 03:13:49 -0700

    await the async function

--- commands/generateachievement.js
+++ commands/generateachievement.js
@@ -12,7 +12,7 @@ module.exports = async (bot, msg, args) => {
     if (msg.author != targets.gideon) return;
     let secret = args[1] == "secret";
     let string = args[1] == "secret" ? msg.content.substring(args[0].length + args[1].length + 2) : msg.content.substring(args[0].length + 1);
-    const buffer = generateAchievementBanner('test', string, secret, true);
+    const buffer = await generateAchievementBanner('test', string, secret, true);
     const image = new MessageAttachment(buffer);
     msg.channel.send(`Here's your test achievement:`, image);
     log.info(`${msg.author.tag} ${msg.author} generated an achievement`);

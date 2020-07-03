03170e5313185af279155dc50a93e53edfa3dd02
Gideon Tong <gideon@gideontong.com>
2020-07-03 10:08:34 -0700

    Update logging

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,8 +1,9 @@
 // Activities are the following:
-// newPresence.activities[0] -> Activity
+// newPresence.activities -> Array<Activity>
 // Activity.type -> {PLAYING, STREAMING, LISTENING, WATCHING, CUSTOM_STATUS}
 // Activity.name -> 1st line, main name
 // Activity.state -> 3rd line in Rich Presence only
+// Activity.details -> main text when type=CUSTOM_STATUS
 module.exports = async (oldPresence, newPresence) => {
-    console.log(newPresence.activities[0].type + ' ' + newPresence.activities[0].name);
+    console.log(newPresence.activities[0].type + ' ' + newPresence.activities[0].name + ' ' + newPresence.activities[0].details);
 }

8c68bdefc3a04429fd3ab7249bf07b00feab2902
Gideon Tong <gideon@gideontong.com>
2020-07-02 23:11:03 -0700

    Add support for presence updates

--- event.js
+++ event.js
@@ -1,10 +1,9 @@
-const guildMemberUpdate = require("./events/guildMemberUpdate");
-
 const reqEvent = (event) => require(`./events/${event}`);
 module.exports = client => {
+    client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
+    client.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
     client.on('message', reqEvent('message'));
     client.on('messageDelete', reqEvent('messageDelete'));
     client.on('messageReactionAdd', reqEvent('messageReactionAdd'));
-    client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
-    client.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
+    client.on('presenceUpdate', reqEvent('presenceUpdate'));
 }

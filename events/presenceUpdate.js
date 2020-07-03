a14e3bb51163a90222237cd4e1bd835e09cb6fb1
Gideon Tong <gideon@gideontong.com>
2020-07-03 10:23:46 -0700

    Prepare leoStatus achievement

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,9 +1,33 @@
+// Imports from local config files
+const config = require('../config/config.json');
+const targets = config.targets;
+
+// Imports from dependencies
+const { grantAchievement } = require('../lib/Achievement');
+const { MessageAttachment } = require('discord.js');
+const log = require('log4js').getLogger('amy');
+
 // Activities are the following:
 // newPresence.activities -> Array<Activity>
 // Activity.type -> {PLAYING, STREAMING, LISTENING, WATCHING, CUSTOM_STATUS}
 // Activity.name -> 1st line, main name
 // Activity.state -> 3rd line in Rich Presence only
-// Activity.details -> main text when type=CUSTOM_STATUS
+// Activity.details -> should be name in CUSTOM_STATUS but not read correctly, do not use
 module.exports = async (oldPresence, newPresence) => {
-    console.log(newPresence.activities[0].type + ' ' + newPresence.activities[0].name + ' ' + newPresence.activities[0].details);
+    if (newPresence.activities && newPresence.activities[0].type == "PLAYING") {
+        let name = newPresence.activities[0].name.toLowerCase();
+        let leo = newPresence.guild.members.cache.get(targets.leo);
+        let channel = newPresence.client.channels.cache.get(targets.general);
+        if (leo) {
+            let nick = leo.nickname;
+            if (name == "leo" || name == nick || name.includes("leo ")) {
+                const buffer = await grantAchievement(newPresence.member.id, 'leoStatus');
+                if (buffer) {
+                    const image = new MessageAttachment(buffer);
+                    channel.send(`<@${newPresence.user.id}> you really did it, the madman...`, image);
+                    log.info(`${newPresence.user.tag} ${newPresence.user} obtained achievement leoStatus`);
+                }
+            }
+        }
+    }
 }

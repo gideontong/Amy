92b83ce5d548f808c3c7ea900aaf031969dd97ce
Gideon Tong <gideon@gideontong.com>
2020-07-02 22:05:32 -0700

    Refactor useAllCommands achievement

--- events/message.js
+++ events/message.js
@@ -28,16 +28,16 @@ module.exports = async message => {
             log.warn(`${message.author.tag} ${message.author} tried to run nonexistent command ${message.content}`);
             return;
         } else {
+            cmdFile(message.client, message, commands).catch(err => {
+                log.error(`${message.author.tag} ${message.author} ran ${message.content} that resulted in error ${err}`);
+            })
             updateStatistic(message.author.id, `use_${toRun}`, 1)
                 .then((value) => {
                     if (value == 1) updateStatistic(message.author.id, 'commands', 1)
                         .then((value) => {
                             if (value == config.constants.uniqueCommands) require('../commands/grantachievement')(message.client, message, ['useAllCommands']);
-                        }
+                        });
                 });
-            cmdFile(message.client, message, commands).catch(err => {
-                log.error(`${message.author.tag} ${message.author} ran ${message.content} that resulted in error ${err}`);
-            })
         }
     }
     sanitizedMessage = message.content.toLowerCase();

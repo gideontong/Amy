e5c40c258f9aa09749c861063a53505407037c23
Gideon Tong <gideon@gideontong.com>
2020-07-03 10:06:50 -0700

    Change logging to type

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,3 +1,8 @@
+// Activities are the following:
+// newPresence.activities[0] -> Activity
+// Activity.type -> {PLAYING, STREAMING, LISTENING, WATCHING, CUSTOM_STATUS}
+// Activity.name -> 1st line, main name
+// Activity.state -> 3rd line in Rich Presence only
 module.exports = async (oldPresence, newPresence) => {
-    console.log(newPresence.activities[0].state + ' ' + newPresence.activities[0].name);
+    console.log(newPresence.activities[0].type + ' ' + newPresence.activities[0].name);
 }

cd6c53b0d403e7ca3110ae70dd3aad026f7c9c66
Gideon Tong <gideon@gideontong.com>
2020-07-02 20:23:57 -0700

    Add support for granting others achievements

--- commands/grantachievement.js
+++ commands/grantachievement.js
@@ -4,16 +4,24 @@ const targets = config.targets;
 
 // Imports from dependencies
 const { grantAchievement } = require('../lib/Achievement');
+const { extractSnowflake } = require('../lib/Validation');
 const { MessageAttachment } = require('discord.js');
 const log = require('log4js').getLogger('amy');
 
 // Wrapper for Achievement.grantAchievement()
 module.exports = async (bot, msg, args) => {
+    var snowflake = msg.author.id;
     if (args[0].startsWith('!')) {
         if (msg.author != targets.gideon) return;
-        else args[0] = args[1];
+        else {
+            args[0] = args[1];
+            if (args[2]) {
+                let sfArg = extractSnowflake(args[2]);
+                if (sfArg) snowflake = sfArg;
+            }
+        }
     }
-    const buffer = await grantAchievement(msg.author.id, args[0]);
+    const buffer = await grantAchievement(snowflake, args[0]);
     if (buffer) {
         log.info(`${msg.author.tag} ${msg.author} achieved achievement ${args[0]}`);
         const image = new MessageAttachment(buffer);

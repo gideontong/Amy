b6916936fc434f7f25037f87d1c5ec27afccbfd9
Gideon Tong <gideon@gideontong.com>
2020-07-02 18:25:32 -0700

    Write everything inbuilt

--- commands/8ball.js
+++ commands/8ball.js
@@ -25,15 +25,30 @@ const responses = [
     "You'll know as soon as quarantine ends."
 ]
 
+// Imports from local config files
+const tips = require('../config/loading.json').messages;
+
 // Imports from dependencies
 const log = require('log4js').getLogger('amy');
+const { selectN } = require('../lib/Today');
 
 // Handler for 8ball command
 module.exports = async (bot, msg, args) => {
-    await require('./loadingscreentips')(bot, msg, args)
-        .then(() => {
-            let response = responses[Math.floor(Math.random() * responses.length)];
-            msg.channel.send(`> ${msg.content.substring(args[0].length + 1)}\n${response}`);
-            log.info(`${msg.author.tag} ${msg.author} requested an 8ball!`);
+    msg.channel.send(`Loading...`)
+        .then(msg => {
+            for (var i = 0; i < messages.length; i++) {
+                writeMessage(msg, messages[i], (i + 1) * 2000);
+            }
+            setTimeout(() => {
+                let response = responses[Math.floor(Math.random() * responses.length)];
+                msg.edit(`> ${msg.content.substring(args[0].length + 1)}\n${response}`);
+            }, (messages.length + 1) * 2000, msg, responses);
         });
+    log.info(`${msg.author.tag} ${msg.author} requested an 8ball!`);
+}
+
+function writeMessage(msg, text, timeout) {
+    setTimeout(() => {
+        msg.edit(`> ${text}\nLoading...`);
+    }, timeout, text)
 }

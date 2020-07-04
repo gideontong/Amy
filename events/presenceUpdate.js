45641e9a3356ddabd82ee7e641e6ae12543ed9bd
Gideon Tong <gideon@gideontong.com>
2020-07-03 20:33:14 -0700

    Put presence into try-catch block

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -14,21 +14,24 @@ const log = require('log4js').getLogger('amy');
 // Activity.state -> 3rd line in Rich Presence only
 // Activity.details -> should be name in CUSTOM_STATUS but not read correctly, do not use
 module.exports = async (oldPresence, newPresence) => {
-    log.info(`Seeing presence activities ${newPresence.activities}`);
-    if (newPresence.activities && newPresence.activities.length > 0) if (newPresence.activities[0].type == "PLAYING") {
-        let name = newPresence.activities[0].name.toLowerCase();
-        let leo = newPresence.guild.members.cache.get(targets.leo);
-        let channel = newPresence.client.channels.cache.get(targets.general);
-        if (leo) {
-            let nick = leo.nickname;
-            if (name == "leo" || name == nick || name.includes("leo ")) {
-                const buffer = await grantAchievement(newPresence.member.id, 'leoStatus');
-                if (buffer) {
-                    const image = new MessageAttachment(buffer);
-                    channel.send(`<@${newPresence.user.id}> you really did it, the madman...`, image);
-                    log.info(`${newPresence.user.tag} ${newPresence.user} obtained achievement leoStatus`);
+    try {
+        if (newPresence.activities && newPresence.activities[0].type == "PLAYING") {
+            let name = newPresence.activities[0].name.toLowerCase();
+            let leo = newPresence.guild.members.cache.get(targets.leo);
+            let channel = newPresence.client.channels.cache.get(targets.general);
+            if (leo) {
+                let nick = leo.nickname;
+                if (name == "leo" || name == nick || name.includes("leo ")) {
+                    const buffer = await grantAchievement(newPresence.member.id, 'leoStatus');
+                    if (buffer) {
+                        const image = new MessageAttachment(buffer);
+                        channel.send(`<@${newPresence.user.id}> you really did it, the madman...`, image);
+                        log.info(`${newPresence.user.tag} ${newPresence.user} obtained achievement leoStatus`);
+                    }
                 }
             }
         }
+    } catch (err) {
+        log.error(`Something weird happened in getting a new presence and I got this instead: ${err}`);
     }
 }

0f1a257a9dae6e7640f6a0649510031eb41b1368
Gideon Tong <gideon@gideontong.com>
2020-07-05 18:11:00 -0700

    Update !downloadvideo to use embed

--- commands/downloadvideo.js
+++ commands/downloadvideo.js
@@ -4,13 +4,28 @@ const { getInfo } = require('youtube-dl');
 
 // downloadvideo handler
 module.exports = async (bot, msg, args) => {
+    if (args.length != 2) return;
     let url = args[1];
-    getInfo(url, [], function(err, data) {
-        if(err) {
+    getInfo(url, [], function (err, data) {
+        if (err) {
             msg.channel.send('Something went wrong, call Gideon for help.');
             log.error(`${msg.author.tag} ${msg.author} ran ${msg.content} that threw ${err}`);
             return;
         }
-        msg.reply(`Here's the download to **${data.title}**: ${data.url}`);
+
+        let embed = {
+            "embed": {
+                "title": "title",
+                "description": `Your download is ready! Click [here](${data.url}) to download your video.\n\n${data.description}`,
+                "url": args[1],
+                "author": {
+                    "name": "Amy's Video Downloader"
+                },
+                "thumbnail": {
+                    "url": data.thumbnail
+                }
+            }
+        };
+        msg.channel.send(embed);
     })
 }

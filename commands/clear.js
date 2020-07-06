77da69963149da125b61f6d737196cabd065d364
Gideon Tong <gideon@gideontong.com>
2020-07-05 18:46:14 -0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -15,11 +15,17 @@ module.exports = async (bot, msg, args) => {
     }
     toClear = toClear > 99 ? 100 : toClear + 1;
     var clearSize = toClear;
-    msg.channel.bulkDelete(toClear, true).then(messages => {
+    msg.channel.bulkDelete(toClear, true)
+    .then(messages => {
         clearSize = messages.size;
         timeout = { "timeout": 3000 };
         msg.channel.send(`${reply} deleted the last ${clearSize} messages.`)
             .then(msg => msg.delete(timeout));
+    })
+    .catch(err => {
+        msg.channel.send(`Didn't delete any messages, you sure there were messages to delete? I can't delete messages more than 2 weeks old using the clear command.`)
+            .then(msg => msg.delete({ "timeout": 3000 }));
+        log.error(`${msg.author.tag} ${msg.author} tried to delete messages but I got ${err}`);
     });
     log.info(`${msg.author.tag} ${msg.author} deleted ${clearSize} messages in ${msg.channel}`)
 }

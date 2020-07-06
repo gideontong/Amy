aa0d57f5951f007f7ce44b142ecee9418cb5797a
Gideon Tong <gideon@gideontong.com>
2020-07-06 11:41:33 -0700

    Actually send the embed :trollface:

--- commands/poll.js
+++ commands/poll.js
@@ -9,13 +9,12 @@ module.exports = async (bot, msg, args) => {
         let question = msg.content.substring(args[0].length + args[1].length + 2);
         let embed = {
             "content": "Hey! Kids it's time to answer a survey!",
-            "embed":
-            {
+            "embed": {
                 "title": "Survey Time!",
                 "description": `${question}\n\nReact with <a:yes:729594115989176332> for yes, and <a:no:729594117017042964> for no!\n\nCurrently we are leaning towards <a:yes:729594115989176332> **yes** with a [coming soon]% vote.`,
                 "color": 10873618
             }
-
-        }
+        };
+        msg.channel.send(embed);
     }
 }

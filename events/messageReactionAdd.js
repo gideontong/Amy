380f2b913025c765cea207e6110c00ba8c18db64
Gideon Tong <gideon@gideontong.com>
2020-07-07 08:02:02 -0700

    messageReactionAdd trigger replaced with Survey.updateExistingPoll

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,26 +1,10 @@
-const { emoji } = require('../config/config.json');
-
 // Dependencies
-const { MessageEmbed } = require("discord.js");
+const { emoji } = require('../config/config.json');
+const { updateExistingPoll } = require('../lib/Survey');
 
 module.exports = async (reaction, user) => {
     let run = reaction.me && reaction.message.editable && !user.bot;
     if (run) {
-        let embed = reaction.message.embeds[0].toJSON();
-        let yesEmoji = reaction.message.reactions.cache.get(emoji.yes);
-        let noEmoji = reaction.message.reactions.cache.get(emoji.no);
-        let yes = yesEmoji ? yesEmoji.count - 1 : 0;
-        let no = noEmoji ? noEmoji.count - 1 : 0;
-        let percentage = Math.round((yes / (yes + no)) * 100);
-        let nextString = embed.fields[2].value;
-        if (yes > no) {
-            nextString = `Currently we are leaning towards <a:yes:${emoji.yes}> **yes** with ${percentage}% of the votes.`;
-        } else if (no > yes) {
-            nextString = `Currently we are leaning towards <a:no:${emoji.no}> **no** with ${100 - percentage}% of the votes.`;
-        } else {
-            nextString = "The votes are currently tied!";
-        }
-        embed.fields[2].value = nextString;
-        reaction.message.edit({ "embed": embed });
+        updateExistingPoll(reaction);
     }
 }

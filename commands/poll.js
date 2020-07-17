f2cfd5ebf4fa108ee07fe0be97a85c53dcfb7a7c
Gideon Tong <gideon@gideontong.com>
2020-07-17 14:38:48 -0700

    Add multiple choice

--- commands/poll.js
+++ commands/poll.js
@@ -18,12 +18,20 @@ const examples = {
         "Roblox",
         "Grant Theft Auto V",
         "VALORANT"
+    ],
+    multipleChoice: [
+        "Russia,Brazil,Indonesia,America;Which country has the lowest number of internet users?",
+        "No,No,Probably Not;Do you like fruitcake?",
+        "Yes,Definitely,Sure,Why Not?;Would you buy a game the Dudes of 708 made?",
+        "Krebs,Calvin,Glycosis,ETC,Cyboxlic Acid Cycle;Which are stages of cell respiration?",
+        "$5,$10,$20;How much money would you like to donate?"
     ]
 }
 
 // Dependencies
 const log = require('log4js').getLogger('amy');
 const { createQuickPoll, createGamePoll } = require('../lib/Survey');
+const { selectN } = require('../lib/Poisson');
 
 // poll quick [question]
 // poll game [times,] [game]
@@ -88,6 +96,7 @@ function createExamples(channel, quick = false, game = false, multipleChoice = f
             }
         }
     };
+    let n = 2;
     if (!(quick && game && multipleChoice)) {
         quick = true, game = true, multipleChoice = true;
     }
@@ -96,6 +105,11 @@ function createExamples(channel, quick = false, game = false, multipleChoice = f
             "name": "Quick Polls",
             "value": "You can make quick polls to ask your friends yes/no questions.\n\nTry asking your friends questions with commands like:\n"
         };
+        let quickExamples = selectN(examples.quick, n);
+        for (var i = 0; i < quickExamples.length; i++) {
+            quickInfo.value += catdance + " `!poll quick " + quickExamples[i] + "`\n";
+        }
+        example.embed.fields.push(quickInfo);
     }
     if (game) {
         let gameInfo = {
@@ -106,11 +120,23 @@ function createExamples(channel, quick = false, game = false, multipleChoice = f
             "name": "Formatting Time",
             "value": "Separate the times you want with commas, and no spaces in between. If you put spaces, Amy will try to figure out what you meant, but she isn't *that* smart yet.\n\n<a:catdance:729440916128923649> `3,4,5` means **3 PM, 4 PM, 5 PM**\n<a:catdance:729440916128923649> `3h,4h,5h` means **in 3 hours, in 4 hours, in 5 hours**. You can also use `m` or `d` for months or days. *It'll automatically be converted into an approximate date and time.*\n<a:catdance:729440916128923649> `3:30,4:30,5:30` means **3:30 PM, 4:30 PM, 5:30 PM**\n\nYou're  also allowed to mix and match, so `3,4:15,8h` is **3 PM, 4:15 PM, in 8 hours**."
         };
+        let gameExamples = selectN(examples.game, n);
+        for (var i = 0; i < gameExamples.length; i++) {
+            gameInfo.value += catdance + " `!poll game [times] " + gameExamples[i] + "`\n";
+        }
+        example.embed.fields.push(gameInfo);
+        example.embed.fields.push(timeInfo);
     }
     if (multipleChoice) {
         let mcInfo = {
             "name": "Multiple Choice Polls",
             "value": "You can make multiple choice polls for additional flexibility. List your options, then a semicolon, then the question, like so:\n\n"
         };
+        let mcExamples = selectN(examples.multipleChoice, n);
+        for (var i = 0; i < mcExamples.length; i++) {
+            mcInfo.value += catdance + " `!poll mc " + mcExamples[i] + "`\n";
+        }
+        example.embed.fields.push(mcInfo);
     }
+    channel.send(example);
 }

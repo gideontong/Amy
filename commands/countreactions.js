60b94c86f7adb2599ed5ba5735b4ba2f8f7ea4d4
Gideon Tong <gideon@gideontong.com>
2020-07-24 16:00:04 -0700

    Print snowflakes of channels

--- commands/countreactions.js
+++ commands/countreactions.js
@@ -1,4 +1,12 @@
+const log = require('log4js').getLogger('amy');
+
 module.exports = async (bot, msg, args) => {
-    let guild = msg.guild;
-    msg.channel.send(guild);
+    let channels = msg.guild.channels; // Collection
+    let channelSnowflakes = channels.array();
+    log.info(channels);
+    let string = "";
+    for (var i = 0; i < channelSnowflakes.length; i++) {
+        string += channelSnowflakes[i] + '\n';
+    }
+    msg.channel.send(string);
 }

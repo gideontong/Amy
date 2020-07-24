806425087893064aa3de268daee41f501df1aab5
Gideon Tong <gideon@gideontong.com>
2020-07-24 16:11:01 -0700

    Fix method call

--- commands/countreactions.js
+++ commands/countreactions.js
@@ -1,8 +1,9 @@
 const log = require('log4js').getLogger('amy');
 
 module.exports = async (bot, msg, args) => {
-    let channels = msg.guild.channels.cache.values(); // Array of GuildChannels
-    let string = "";
+    let channels = msg.guild.channels.cache.array(); // Array of GuildChannels
+    log.info(channels);
+    let string = "check log\n";
     for (var i = 0; i < channels.length; i++) {
         string += channels[i].name + " " + channels[i].type + '\n';
     }

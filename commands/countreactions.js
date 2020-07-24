d70e315677a0899dd126d523a8bfa78e6dd9f8ee
Gideon Tong <gideon@gideontong.com>
2020-07-24 16:14:19 -0700

    Print only text channels

--- commands/countreactions.js
+++ commands/countreactions.js
@@ -2,10 +2,11 @@ const log = require('log4js').getLogger('amy');
 
 module.exports = async (bot, msg, args) => {
     let channels = msg.guild.channels.cache.array(); // Array of GuildChannels
-    log.info(channels);
-    let string = "check log\n";
+    let string = "__Quick Response__\n";
     for (var i = 0; i < channels.length; i++) {
-        string += channels[i].name + " " + channels[i].type + '\n';
+        if (channels[i].type == "text") {
+            string += channels[i].name + '\n';
+        }
     }
     msg.channel.send(string);
 }

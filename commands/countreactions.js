7cc1f46aef11adc63710ec2491635b198c4385ce
Gideon Tong <gideon@gideontong.com>
2020-07-24 16:08:39 -0700

    Values, not keys. It's a map

--- commands/countreactions.js
+++ commands/countreactions.js
@@ -1,7 +1,10 @@
 const log = require('log4js').getLogger('amy');
 
 module.exports = async (bot, msg, args) => {
-    let channels = Array.from(msg.guild.channels.cache.keys()); // Array of GuildChannels
-    log.info(channels);
-    msg.channel.send("check log");
+    let channels = msg.guild.channels.cache.values(); // Array of GuildChannels
+    let string = "";
+    for (var i = 0; i < channels.length; i++) {
+        string += channels[i].name + " " + channels[i].type + '\n';
+    }
+    msg.channel.send(string);
 }

e4992a5c8190ecfcc932e8d656f749bd8f0f8f6d
Gideon Tong <gideon@gideontong.com>
2020-07-24 16:06:15 -0700

    De-cache array

--- commands/countreactions.js
+++ commands/countreactions.js
@@ -1,7 +1,7 @@
 const log = require('log4js').getLogger('amy');
 
 module.exports = async (bot, msg, args) => {
-    let channels = msg.guild.channels.cache.keyArray(); // Array of GuildChannels
+    let channels = Array.from(msg.guild.channels.cache.keys()); // Array of GuildChannels
     let string = "";
     for (var i = 0; i < channels.length; i++) {
         string += channels[i].name + " " + channels[i].type + '\n';

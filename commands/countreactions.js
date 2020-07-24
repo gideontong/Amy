64e455acbb18eefd12ac59ceff3ee35652bc73a4
Gideon Tong <gideon@gideontong.com>
2020-07-24 16:03:47 -0700

    Now print the names of all the channels and their type

--- commands/countreactions.js
+++ commands/countreactions.js
@@ -1,12 +1,10 @@
 const log = require('log4js').getLogger('amy');
 
 module.exports = async (bot, msg, args) => {
-    let channels = msg.guild.channels; // Collection
-    let channelSnowflakes = channels.cache.array();
-    log.info(channels);
+    let channels = msg.guild.channels.cache.keyArray(); // Array of GuildChannels
     let string = "";
-    for (var i = 0; i < channelSnowflakes.length; i++) {
-        string += channelSnowflakes[i] + '\n';
+    for (var i = 0; i < channels.length; i++) {
+        string += channels[i].name + " " + channels[i].type + '\n';
     }
     msg.channel.send(string);
 }

bda942a328acb3a1be6826c89320e20f046fa80f
Gideon Tong <gideon@gideontong.com>
2020-08-01 17:21:00 -0700

    Add riotlogout command

---
+++ commands/riotlogout.js
@@ -0,0 +1,3 @@
+module.exports = async (bot, msg, args) => {
+    msg.channel.send("Your Riot Games data was successfully deleted from Amy's servers. This decision is permanent, but you can sign in again if you want to come back!");
+}

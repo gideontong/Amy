74d36502da3c515b282f51b1c74fa7189b45d8a7
Gideon Tong <gideon@gideontong.com>
2020-09-21 02:25:59 -0700

    Disable new members for now

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,8 @@
-// Import from local config files
-const config = require('../config/config.json');
-const strings = config.strings;
-
 // Imports from dependencies
 const log = require('log4js').getLogger('amy');
 
 // Handler for a new member added
 module.exports = async member => {
     if (member.user.bot) return;
-    member.send(strings.newMember);
     log.info(`${member.user.tag} ${member} joined the server`);
 }

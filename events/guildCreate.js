3a1460a72a2f66cf9956a64d6bb056e4518900b4
Gideon Tong <gideon@gideontong.com>
2020-11-24 19:05:11 -0800

    Update guildCreate to new format

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,22 +1,20 @@
-const { snowflakes } = require('../config/config.json');
+const { logging } = require('../config/config.json').channels;
 const { MessageEmbed } = require('discord.js');
 const log = require('log4js').getLogger('amy');
 
 module.exports = async guild => {
+    const client = guild.client;
     log.info(`Joined new guild ${guild.name}`);
     try {
         if (guild.available) {
-            let loggerGuild = guild.client.guilds.resolve(snowflakes.guilds.logging);
-            if (loggerGuild) {
-                let channel = loggerGuild.channels.resolve(snowflakes.channels.logging);
-                if (channel && channel.type == 'text') {
-                    const guildNotifcation = new MessageEmbed()
-                        .setAuthor(guild.owner.user.tag, guild.owner.user.displayAvatarURL())
-                        .setTitle(`Joined new server ${guild.name}`)
-                        .setDescription(`Has ${guild.memberCount} members and was originally created on ${guild.createdAt}`)
-                        .setFooter(`Joined at ${guild.joinedAt}`);
-                    channel.send(guildNotifcation);
-                }
+            let channel = client.channels.cache.get(logging);
+            if (channel && channel.type == 'text') {
+                const notification = new MessageEmbed()
+                    .setAuthor(guild.owner.user.tag, guild.owner.user.displayAvatarURL())
+                    .setTitle(`Joined new server ${guild.name}`)
+                    .setDescription(`Has ${guild.memberCount} members and was originally created on ${guild.createdAt}`)
+                    .setFooter(`Joined at ${guild.joinedAt}`);
+                channel.send(notification);
             }
         }
     } catch (err) {

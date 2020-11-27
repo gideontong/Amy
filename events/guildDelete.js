d965685dbf1138ea44565910f300cbce7287dc90
Gideon Tong <gideon@gideontong.com>
2020-11-27 15:07:25 -0800

    Fix bug in guildDelete

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -10,14 +10,16 @@ module.exports = async guild => {
     log.info(`Left guild ${guild.name}`);
     try {
         if (guild.available) {
-            let channel = guild.client.channels.cache.get(logging);
-            if (channel && channel.type == 'text') {
-                const notification = new MessageEmbed()
-                    .setAuthor(guild.owner.user.tag, guild.owner.user.displayAvatarURL())
-                    .setTitle(`Left server ${guild.name}`)
-                    .setDescription(`Has ${guild.memberCount} members and was originally created on ${guild.createdAt}`)
-                    .setFooter(`Originally joined at ${guild.joinedAt}`);
-                channel.send(notification);
+            const notification = new MessageEmbed()
+                .setAuthor(guild.owner.user.tag, guild.owner.user.displayAvatarURL())
+                .setTitle(`Left server ${guild.name}`)
+                .setDescription(`Has ${guild.memberCount} members and was originally created on ${guild.createdAt}`)
+                .setFooter(`Originally joined at ${guild.joinedAt}`);
+            for (channelID of logging) {
+                let channel = guild.client.channels.cache.get(channelID);
+                if (channel && channel.type == 'text') {
+                    channel.send(notification);
+                }
             }
         }
     } catch (err) {

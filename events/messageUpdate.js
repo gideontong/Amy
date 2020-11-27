a2108b03dec48301ceb8e4513501ac4760e83be9
Gideon Tong <gideon@gideontong.com>
2020-11-27 15:25:30 -0800

    Complete messageUpdate update

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,31 +1,34 @@
-const { snowflakes } = require('../config/config.json');
-
+const { logging } = require('../config/config.json').channels;
 const { MessageEmbed } = require('discord.js');
 const log = require('log4js').getLogger('amy');
 
+/**
+ * Sends an alert whenever a message is edited
+ * @param {Message} oldMessage Message before it was edited
+ * @param {Message} newMessage Message after it was edited
+ */
 module.exports = async (oldMessage, newMessage) => {
-    if (oldMessage.author.bot || newMessage.author.bot || !newMessage.editedAt) return;
-    let original = oldMessage.cleanContent;
-    let updated = newMessage.cleanContent;
-    log.info(`${oldMessage.author.tag} edited ${original} to ${updated} in ${oldMessage.guild.name} (${oldMessage.channel.name})`);
-    let guild = oldMessage.client.guilds.resolve(snowflakes.guilds.logging);
+    if (newMessage.author.bot || !newMessage.editedAt) return;
     try {
-        if (guild.available) {
-            let channel = guild.channels.resolve(snowflakes.channels.logging);
+        log.info(`${oldMessage.author.tag} edited ${newMessage.id} in ${oldMessage.guild.name} (${oldMessage.channel.name})`);
+        let original = oldMessage.cleanContent;
+        let updated = newMessage.cleanContent;
+        if (original.length > 500) {
+            original = original.substring(0, 500) + '...';
+        }
+        if (updated.length > 500) {
+            updated = updated.substring(0, 500) + '...';
+        }
+        const editedComment = new MessageEmbed()
+            .setAuthor(oldMessage.author.tag, oldMessage.author.displayAvatarURL())
+            .setTitle(`Message edited in ${oldMessage.guild.name}!`)
+            .setDescription(`It had ${oldMessage.attachments.size} attachments.`)
+            .addField('Old Message', original, false)
+            .addField('New Message', updated, false)
+            .setFooter(`Edited at ${newMessage.editedAt} in ${oldMessage.channel.name}`);
+        for (channelID of logging) {
+            let channel = newMessage.client.channels.cache.get(channelID);
             if (channel && channel.type == 'text') {
-                if (original.length > 500) {
-                    original = original.substring(0, 500) + '...';
-                }
-                if (updated.length > 500) {
-                    updated = updated.substring(0, 500) + '...';
-                }
-                const editedComment = new MessageEmbed()
-                    .setAuthor(oldMessage.author.tag, oldMessage.author.displayAvatarURL())
-                    .setTitle(`Message edited in ${oldMessage.guild.name}!`)
-                    .setDescription(`It had ${oldMessage.attachments.size} attachments.`)
-                    .addField('Old Message', original, false)
-                    .addField('New Message', updated, false)
-                    .setFooter(`Edited at ${newMessage.editedAt} in ${oldMessage.channel.name}`);
                 channel.send(editedComment);
             }
         }

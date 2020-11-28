dfaa8ed434877f9729e17118841c1e0bb1facfb3
Gideon Tong <gideon@gideontong.com>
2020-11-28 13:23:10 -0800

    Add emoji as printemoji alias

---
+++ commands/emoji.js
@@ -0,0 +1,9 @@
+/**
+ * Alias for printemoji command
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
+    require('./printemoji')(client, msg, args);
+}

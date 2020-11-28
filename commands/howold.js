5d7b93ce98af5d9651c87ca3c6412fdbd2863c05
Gideon Tong <gideon@gideontong.com>
2020-11-28 13:03:43 -0800

    Update JSDoc for howold command

--- commands/howold.js
+++ commands/howold.js
@@ -1,4 +1,9 @@
-// Running howold runs accountage
-module.exports = async (bot, msg, args) => {
-    require('./accountage')(bot, msg, args);
+/**
+ * Alias for accountage command
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
+    require('./accountage')(client, msg, args);
 }

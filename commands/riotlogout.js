a230500aeeba5cfb819929911ba6eeba4dbc7a8e
Gideon Tong <gideon@gideontong.com>
2020-11-28 14:07:07 -0800

    Deprecate riot commands

--- commands/riotlogout.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (bot, msg, args) => {
-    msg.channel.send("Your Riot Games data was successfully deleted from Amy's servers. This decision is permanent, but you can sign in again if you want to come back!");
-}

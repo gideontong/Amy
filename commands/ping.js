ffde9fbc5a3018d8077354045c050659eab509c8
Gideon Tong <gideon@gideontong.com>
2020-11-28 13:18:20 -0800

    Pings a server

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,38 @@
-// Handler for running ping command
-module.exports = async (bot, msg, args) => {
-    msg.channel.send("We don't ping Discord around here! Try asking <@290514680902975488> by saying `ping` or asking <@727603104807256168> by saying `bing`!");
+/**
+ * Creates a ping test and sends it to the server
+ * @param {Client} client Discord client
+ * @param {Message} msg Command
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
+    msg.channel.send('🏓 Pinging...').then((message) => {
+        const start = new Date().valueOf();
+        const config = require('../config/config.json');
+        const end = new Date().valueOf();
+        message.edit({
+            content: "🏓 Successfully ponged!",
+            embed: {
+                title: `📶 ${client.user.username} Service Availability`,
+                description: `Currently running on \`us-west-01.gid.network/discord\`. Issues? Contact [Gideon Tong](https://gideontong.com) for help.`,
+                fields: [
+                    {
+                        name: "Server Round Trip Time",
+                        value: `${message.createdAt - msg.createdAt}ms`
+                    },
+                    {
+                        name: "Server Connection Time",
+                        value: `${client.ws.ping}ms`
+                    },
+                    {
+                        name: "Database Connection Time",
+                        value: `${end - start}ms`
+                    },
+                ],
+                footer: {
+                    text: `Currently serving ${client.users.cache.size} users!`
+                },
+                color: 0x18ffff
+            }
+        });
+    })
 }

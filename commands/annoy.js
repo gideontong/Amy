5299bd075fc6568963f936c2b61b6f3ed1a0509a
Gideon Tong <gideon@gideontong.com>
2020-11-28 12:44:12 -0800

    Update annoy command to new format

--- commands/annoy.js
+++ commands/annoy.js
@@ -1,25 +1,18 @@
-// Imports from local config files
-const config = require('../config/config.json');
-
-// Imports from dependencies
-const log = require('log4js').getLogger('amy');
 const { extractSnowflake } = require('../lib/Validation');
 
-// Handler for running annoy command
-module.exports = async (bot, msg, args) => {
-    var id = msg.author.id;
+/**
+ * Pings a desired person with an annoying message
+ * @param {Client} client Discord server client
+ * @param {Message} msg Message to execute
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
     if (args.length < 2) {
         msg.reply('imagine not trying to annoy someone else? HAHAHA');
-    } else if (args.length > 2) {
-        msg.reply('run `!annoy (user)` to annoy them!');
     } else {
         const annoy = extractSnowflake(args[1]);
         if (annoy) {
-            const link = Math.random() < 0.3 ? config.links.troll : "https://www.youtube.com/watch?v=VGBO663oenw";
-            msg.channel.send(`Hey, <@${annoy}>! I was told to be annoying to you, so watch this video: ${link}`);
-            require('./grantachievement')(bot, msg, ['annoying']);
-            if (annoy == msg.author.id) require('./grantachievement')(bot, msg, ['selfAnnoy']);
-            log.info(`${msg.author.tag} ${msg.author} decided to annoy <@${annoy}>`);
+            msg.channel.send(`Hey, <@${annoy}>! Your breath smells!`);
         } else {
             msg.reply('that is not a person...?');
         }

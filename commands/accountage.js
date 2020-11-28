679fa8073700ce2adc6ba3cf176c1fef2234953c
Gideon Tong <gideon@gideontong.com>
2020-11-27 16:17:27 -0800

    Format and JSDoc accountage command

--- commands/accountage.js
+++ commands/accountage.js
@@ -1,18 +1,17 @@
-// Imports from dependencies
 const { timeSinceSnowflake } = require('../lib/Today');
 const { validSnowflake } = require('../lib/Validation');
-const log = require('log4js').getLogger('amy')
 const { prefix } = require('../config/config.json');
+const log = require('log4js').getLogger('amy')
 
-// See joined command for more info
-// Handler for running accountage command
-module.exports = async (bot, msg, args) => {
+/**
+ * Returns the account age of an account
+ * @param {Client} client Discord bot client
+ * @param {Message} msg Message emitted
+ * @param {Array} args Arguments to parse
+ */
+module.exports = async (client, msg, args) => {
     var id = msg.author.id;
     if (args.length > 1) {
-        if (args.length > 2) {
-            msg.reply(`run \`${prefix} accountage (ID)\` to get your account age!`);
-            return;
-        }
         id = (!isNaN(args[1])) ? args[1] : args[1].substring(3, args[1].length - 1);
     }
     if (validSnowflake(id)) {

94dbf605fb61c34f308cdb064e5c66a9647aa4cb
Gideon Tong <gideon@gideontong.com>
2020-11-28 13:26:25 -0800

    Add support to printemoji for emoji names

--- commands/printemoji.js
+++ commands/printemoji.js
@@ -7,8 +7,18 @@ const log = require('log4js').getLogger('amy');
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
-    // TODO: add support for emoji names
-    var emoji = client.emojis.resolve(args[1]);
+    var emoji;
+    if (isNaN(args[1])) {
+        let search = args[1].toLowerCase();
+        let emojis = client.emoji.cache.array();
+        for (potential of emojis) {
+            if (search == potential.name.toLowerCase()) {
+                emoji = potential;
+            }
+        }
+    } else {
+        emoji = client.emojis.resolve(args[1]);
+    }
     if (emoji) {
         msg.channel.send(emoji.toString());
         log.info(`${msg.author.tag} ${msg.author} told me to repeat :${emoji.name}: ${emoji.animated? '(animated)': ''}`);

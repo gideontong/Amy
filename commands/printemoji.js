45e792029d400146c66db378e3363655460ebddb
Gideon Tong <gideon@gideontong.com>
2020-11-28 13:22:45 -0800

    Complete and enable printemoji for everyone to use

--- commands/printemoji.js
+++ commands/printemoji.js
@@ -1,14 +1,14 @@
-// Imports from local config files
-const config = require('../config/config.json');
-const targets = config.targets;
-
-// Imports from dependencies
 const log = require('log4js').getLogger('amy');
 
-// Handler for running printemoji command
-module.exports = async (bot, msg, args) => {
+/**
+ * Prints an emoji, regardless of whether it was animated or not
+ * @param {Client} client Discord client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
     // TODO: add support for emoji names
-    var emoji = bot.emojis.resolve(args[1]);
+    var emoji = client.emojis.resolve(args[1]);
     if (emoji) {
         msg.channel.send(emoji.toString());
         log.info(`${msg.author.tag} ${msg.author} told me to repeat :${emoji.name}: ${emoji.animated? '(animated)': ''}`);

26e8a1b66a3ffac896b09488ccaf4415214ba6c5
Gideon Tong <gideon@gideontong.com>
2020-11-28 12:56:53 -0800

    Update and add JSDoc for donate command

--- commands/donate.js
+++ commands/donate.js
@@ -1,11 +1,11 @@
-// Imports from local config files
-const config = require('../config/config.json');
+const link = 'https://ko-fi.com/gideontong';
 
-// Imports from dependencies
-const log = require('log4js').getLogger('amy');
-
-// Handler for running donate command
-module.exports = async (bot, msg, args) => {
-    msg.channel.send(config.links.donate);
-    log.info(`${msg.author.tag} ${msg.author} requested the donate link`);
+/**
+ * Donate command handler
+ * @param {Client} client Discord server client
+ * @param {Message} msg Message to execute
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
+    msg.channel.send(`You can donate to support development here: ${link}`);
 }

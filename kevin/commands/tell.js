396a82eceb37f935065ef0b30b263d546bf4d376
Gideon Tong <gideon@gideontong.com>
2020-11-28 17:50:59 -0800

    Wrap tell command in try-catch

--- kevin/commands/tell.js
+++ kevin/commands/tell.js
@@ -8,17 +8,21 @@ const log = require('log4js').getLogger('kevin');
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
-    if (args.length > 2 && validChannel(args[1])) {
-        const snowflake = extractSnowflake(args[1]);
-        if (snowflake) {
-            const channel = msg.client.channels.cache.get(snowflake);
-            channel.send(msg.content.substring(args[0].length + args[1].length + 2));
-            log.info(`${msg.author.tag} ${msg.author} told me to send a message in #${channel.name}`);
-            return;
+    try {
+        if (args.length > 2 && validChannel(args[1])) {
+            const snowflake = extractSnowflake(args[1]);
+            if (snowflake) {
+                const channel = msg.client.channels.cache.get(snowflake);
+                channel.send(msg.content.substring(args[0].length + args[1].length + 2));
+                log.info(`${msg.author.tag} ${msg.author} told me to send a message in #${channel.name}`);
+                return;
+            }
+        } else {
+            msg.channel.send(msg.content.substring(args[0].length + 1));
+            msg.delete();
+            log.info(`${msg.author.tag} ${msg.author} told me to send a message`);
         }
-    } else {
-        msg.channel.send(msg.content.substring(args[0].length + 1));
-        msg.delete();
-        log.info(`${msg.author.tag} ${msg.author} told me to send a message`);
+    } catch (err) {
+        log.error(`Something happened while trying to repeat a message: ${err}`);
     }
 }

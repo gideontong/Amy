ef439f21697f9273305545c08b1f1482aab5be4d
Gideon Tong <gideon@gideontong.com>
2020-11-29 14:26:25 -0800

    Fix bugs

--- commands/quote.js
+++ commands/quote.js
@@ -11,6 +11,8 @@ const { MessageEmbed } = require('discord.js');
 module.exports = async (client, msg, args) => {
     let index = Math.floor(Math.random() * quotes.length);
     const color = Math.floor(Math.random() * colors);
+    const quote = new MessageEmbed()
+        .setColor(color);
     if (args[1] && !isNaN(args[1])) {
         let idx = parseInt(args[1]) - 1;
         if (idx < quotes.length) {
@@ -19,9 +21,7 @@ module.exports = async (client, msg, args) => {
             quote.addField('Error', `There are only ${quotes.length} quotes in the database, so a random one was used.`);
         }
     }
-    const quote = new MessageEmbed()
-        .setColor(color)
-        .setDescription(`*${quotes[index].text}*`)
+    quote.setDescription(`*${quotes[index].text}*`)
         .setFooter(`- ${quotes[index].author}`)
         .setTitle(`Random Quote #${index + 1}`);
     msg.channel.send(quote);

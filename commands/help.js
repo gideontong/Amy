65b24966286789f05846b7b49d56f74a43e636b6
Gideon Tong <gideon@gideontong.com>
2020-11-29 00:55:10 -0800

    Fix help command typo

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const helpEmbed = {
     "title": "Need help? Never fear, Amy is here!",
     "description": "**Amy** is your personal assistant and here to save the day.\n\nTo see a full list of commands you can use, [click here](https://github.com/gideontong/Amy/blob/master/docs/usage/README.md).\n\n[Amy's Website](https://amyhelps.ml) • [Full Help Screen](https://github.com/gideontong/Amy/blob/master/docs/README.md) • [Donate](https://ko-fi.com/gideontong)",
     "footer": {
-        "text": "!help always calls the help command."
+        "text": "~help always calls the help command."
     },
     "thumbnail": {
         "url": "http://animaticons.co/wp-content/uploads/animat-image-256x256-color.gif"

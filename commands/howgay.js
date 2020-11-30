85ddccea036ff3aa5d2144445fce208238c07129
Gideon Tong <gideon@gideontong.com>
2020-11-30 11:10:48 -0800

    Update how gay

--- commands/howgay.js
+++ commands/howgay.js
@@ -1,3 +1,6 @@
+const colors = 0xFFFFFF;
+const { MessageEmbed } = require('discord.js');
+
 /**
  * How gay are you?
  * @param {Client} client Discord server client
@@ -5,5 +8,23 @@
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
-    msg.reply(`you are ${Math.floor(Math.random() * 100)}% gay!`);
+    let mentions = msg.mentions.members;
+    if (mentions) {
+        let members = mentions.array();
+        if (mentions.size == 0) {
+            msg.reply(`you are ${Math.floor(Math.random() * 100)}% gay!`);
+        } else if (mentions.size == 1) {
+            msg.channel.send(`${members[0].toString()} is ${Math.floor(Math.random() * 100)}% gay!`);
+        } else {
+            let description = '';
+            for (member of members) {
+                description += `${member.toString()} is ${Math.floor(Math.random() * 100)}% gay!\n`;
+            }
+            const embed = new MessageEmbed()
+                .setColor(Math.floor(Math.random() * colors))
+                .setDescription(description)
+                .setTitle('Gayness Table');
+            msg.channel.send(embed);
+        }
+    }
 }

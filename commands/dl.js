5d9ebac80bda1b7abf0d4a1ec23e473423a54c5a
Gideon Tong <gideon@gideontong.com>
2020-11-30 10:28:46 -0800

    Add aliases for downloadvideo command

---
+++ commands/dl.js
@@ -0,0 +1,9 @@
+/**
+ * Alias for downloadvideo command
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
+    require('./downloadvideo')(client, msg, args);
+}

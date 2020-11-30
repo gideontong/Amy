f50c34adb280be2d26527adfa38e01fda91dcfa8
Gideon Tong <gideon@gideontong.com>
2020-11-30 11:12:01 -0800

    Update howstraight to be like howgay

--- commands/howstraight.js
+++ commands/howstraight.js
@@ -1,3 +1,6 @@
+const colors = 0xFFFFFF;
+const { MessageEmbed } = require('discord.js');
+
 /**
  * How straight are you?
  * @param {Client} client Discord server client
@@ -5,5 +8,23 @@
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
-    msg.reply(`you are ${Math.floor(Math.random() * 100)}% straight!`);
+    let mentions = msg.mentions.members;
+    if (mentions) {
+        let members = mentions.array();
+        if (mentions.size == 0) {
+            msg.reply(`you are ${Math.floor(Math.random() * 100)}% straight!`);
+        } else if (mentions.size == 1) {
+            msg.channel.send(`${members[0].toString()} is ${Math.floor(Math.random() * 100)}% straight!`);
+        } else {
+            let description = '';
+            for (member of members) {
+                description += `${member.toString()} is ${Math.floor(Math.random() * 100)}% straight!\n`;
+            }
+            const embed = new MessageEmbed()
+                .setColor(Math.floor(Math.random() * colors))
+                .setDescription(description)
+                .setTitle('Hotness Table');
+            msg.channel.send(embed);
+        }
+    }
 }

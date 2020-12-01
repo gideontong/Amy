f8092f3d8007f1c5dd84587199780708191a46cf
Gideon Tong <gideon@gideontong.com>
2020-11-30 20:31:55 -0800

    Big fix

--- commands/urbandictionary.js
+++ commands/urbandictionary.js
@@ -20,9 +20,10 @@ module.exports = async (client, msg, args) => {
                 'term': args.join(' ')
             }, function(data) {
                 if (data.list.length > 0) {
+                    const definition = data.list[0].definition.length > 3000 ? data.list[0].definition.substr(0, 3000) : data.list[0].definition;
                     let definition = new MessageEmbed()
                         .setColor(Math.floor(Math.random() * colors))
-                        .setDescription(data.list[0].definition)
+                        .setDescription(definition)
                         .setFooter(`${data.list[0].thumbs_up} likes`)
                         .setTitle(data.list[0].word);
                     msg.channel.send(definition);

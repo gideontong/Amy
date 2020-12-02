a4e61ce433008da54e0ad09d6bd0d8c32c5958f9
Gideon Tong <gideon@gideontong.com>
2020-12-02 12:50:39 -0800

    Add a bad feature

---
+++ commands/hentai.js
@@ -0,0 +1,31 @@
+// You are a sick person. This list was auto-generated, and I
+// don't want to see it ever again.
+const subreddits = [
+    'hentai',
+    'ecchi',
+    'awwnime',
+    'hentaimemes'
+];
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Undocumented command, returns a random NSFW image
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    if (msg.channel.type == 'text' && msg.channel.nsfw) {
+        try {
+            getRedditImage(function (data = 'Loading...') {
+                msg.channel.send(data);
+            }, subreddits);
+        } catch (err) {
+            log.error(`While trying to grab a Reddit NSFW picture I got ${err}`);
+        }
+    } else {
+        msg.channel.send('NSFW images are only allowed in NSFW channels... you sick bastard.');
+    }
+}

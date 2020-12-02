4dfff7d07866e67fcc04a681fe6de18c02c82178
Gideon Tong <gideon@gideontong.com>
2020-12-01 21:08:52 -0800

    Attempt a bugfix

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -10,16 +10,17 @@ const log = require('log4js').getLogger('amy');
 module.exports = async (oldMember, newMember) => {
     if (newMember.guild.available && guilds.enabled.includes(newMember.guild.id)) {
         if (newMember.nickname) {
-            log.info(`${newMember.user.tag} updated their nickname to ${newMember.nickname}`);
             let message = new MessageEmbed()
                 .setAuthor(newMember.user.tag, newMember.user.displayAvatarURL())
                 .setColor('GOLD')
                 .setTitle('Nickname was updated!');
-            if (oldMember.nickname && oldMember.nickname != newMember.nickname) {
+            if (oldMember.nickname) {
+                if (oldMember.nickname == newMember.nickname) return;
                 message.setDescription(`Nickname was changed from ${oldMember.nickname} to ${newMember.nickname}.`);
             } else {
                 message.setDescription(`A new nickname was added, which was ${newMember.nickname}.`);
             }
+            log.info(`${newMember.user.tag} updated their nickname to ${newMember.nickname}`);
             for (channelID of channels.alerts) {
                 let channel = newMember.client.channels.cache.get(channelID);
                 if (channel && channel.type == 'text') {

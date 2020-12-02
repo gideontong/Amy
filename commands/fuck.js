5a5436a72d7f1fa328003482e04244107f551337
Gideon Tong <gideon@gideontong.com>
2020-12-02 11:40:47 -0800

    Watch for food command spam

--- commands/fuck.js
+++ commands/fuck.js
@@ -1,37 +1,23 @@
 const host = 'www.reddit.com';
-// Have a suggestion? File a pull request or contact Gideon#5433!
+// You are a sick person. This list was auto-generated, and I
+// don't want to see it ever again.
 const subreddits = [
-    'beerandpizza',
-    'beerfood',
-    'beerporn',
-    'bobsburgerscreations',
-    'burgers',
-    'cheeseburgers',
-    'food',
-    'foodporn',
-    'friedchicken',
-    'knightsofpineapple',
-    'microwavepics',
-    'pizza',
-    'putaneggonit',
-    'ramen',
-    'salsasnobs',
-    'sexypizza',
-    'streeteats',
-    'streetfoodartists',
-    'sushi',
-    'swedishfood',
-    'tacos',
-    'tonightsdinner',
-    'tonightsvegdinner',
-    'wings'
+    '18_19',
+    'asianhotties',
+    'asiansgonewild',
+    'collegesluts',
+    'celebnsfw',
+    'girlsinyogapants',
+    'nsfw',
+    'palegirls',
+    'realgirls'
 ];
 
 const { authenticatedGet } = require('../lib/Internet');
 const log = require('log4js').getLogger('amy');
 
 /**
- * Gets a random photo from a food subreddit
+ * Undocumented command, returns a random NSFW image
  * @param {Client} client Discord server client
  * @param {Message} msg Command
  * @param {Array} args Arguments
@@ -49,19 +35,18 @@ module.exports = async (client, msg, args) => {
                 && data[0].data.children[0].data
                 && data[0].data.children[0].data.url_overridden_by_dest) {
                 const link = data[0].data.children[0].data.url_overridden_by_dest;
-                if (link.startsWith('https://i.')) {
-                    msg.channel.send(link);
+                if (link.startsWith('https://www.reddit.com')) {
+                    msg.channel.send(`Check out this Reddit gallery (auto-gallery coming soon!): ${link}`);
                 } else {
-                    msg.channel.send(`I got a link, but I don't support this yet, so you can view it in the web browser: ${link}`);
+                    msg.channel.send(link);
                 }
             } else {
-                log.warn('Food command is calling itself and going down call stack!');
-                require('./food')(client, msg, args);
+                msg.channel.send("That didn't work... try again?");
             }
         }, host, endpoint, {}, {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
         });
     } catch (err) {
-        log.error(`While trying to grab a Reddit food picture I got ${err}`);
+        log.error(`While trying to grab a Reddit battlestation I got ${err}`);
     }
 }

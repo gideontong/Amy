3d71044a1a2982b702adbf2b9f95bd91dd720fb1
Gideon Tong <gideon@gideontong.com>
2020-12-03 11:46:01 -0800

    Alias stockprice with ticker

---
+++ commands/ticker.js
@@ -0,0 +1,9 @@
+/**
+ * Alias for stockprice command
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Command arguments
+ */
+module.exports = async (client, msg, args) => {
+    require('./stockprice')(client, msg, args);
+}

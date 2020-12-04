7567e2b16aa1ddd922f42faab7c417ffe6084ba5
Gideon Tong <gideon@gideontong.com>
2020-12-03 19:00:48 -0800

    Add EarthPorn command

---
+++ commands/earthporn.js
@@ -0,0 +1,20 @@
+const subreddit = 'earthporn';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/earthporn
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/earthporn picture I got ${err}`);
+    }
+}

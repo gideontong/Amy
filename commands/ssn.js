a2b292b13179499ca2b0a182371f0559322b1789
Gideon Tong <gideon@gideontong.com>
2020-12-03 21:01:04 -0800

    Add SSN command

---
+++ commands/ssn.js
@@ -0,0 +1,21 @@
+const host = 'www.ssn-check.org';
+const endpoint = '/api/generate';
+
+const { authenticatedPost } = require('../lib/Internet');
+
+/**
+ * Get a randomly generated SSN
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    authenticatedPost(function (data) {
+        if (data && data['ssn']) {
+            msg.channel.send(`Your fake ${data['state'] ? data['state'] : 'American'} SSN is: ${data['ssn']}`)
+        }
+    }, host, endpoint, {
+        'state': '',
+        'year': 0
+    });
+}

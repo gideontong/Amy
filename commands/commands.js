00da6132e5f53684ce5608d35e3b496f179e9a3e
Gideon Tong <gideon@gideontong.com>
2020-12-03 20:14:59 -0800

    Update commands listing code

--- commands/commands.js
+++ commands/commands.js
@@ -1,4 +1,8 @@
+const colors = 0xFFFFFF;
+const perPage = 7;
+
 const { commands } = require('../config/commands.json');
+const { MessageEmbed } = require('discord.js');
 
 /**
  * List the commands avilable on the bot
@@ -7,9 +11,24 @@ const { commands } = require('../config/commands.json');
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
+    var page = 1;
+    var start = 0;
+    if (args.length > 1 && !isNaN(args[1])) {
+        let value = parseInt(args[1]);
+        let begin = (value - 1) * perPage;
+        if (begin < commands.length) {
+            page = value;
+            start = begin;
+        }
+    }
     let description = '';
-    for (var i = 0; i < 5; i++) {
+    let embed = new MessageEmbed()
+        .setAuthor(msg.member.nickname ? msg.member.nickname : client.user.username, client.user.displayAvatarURL())
+        .setColor(Math.floor(Math.random() * colors))
+        .setFooter(`Page ${page} of ${Math.floor(commands.length / perPage) + 1}`);
+    for (var i = start; i < start + perPage; i++) {
         description += `**${commands[i].command}**: ${commands[i].description}\n`
     }
-    msg.channel.send(description);
+    embed.setDescription(description);
+    msg.channel.send(embed);
 }

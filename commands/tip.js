bf29ff13bdd6970e8828b46cd99b71d22d972b05
Gideon Tong <gideon@gideontong.com>
2020-12-05 22:51:01 -0800

    Add tip commands

--- commands/tip.js
+++ commands/tip.js
@@ -1,26 +1,26 @@
 const colors = 0xFFFFFF;
-const { quotes } = require('../config/fun.json');
+const { tips } = require('../config/fun.json');
 const { MessageEmbed } = require('discord.js');
 
 /**
- * Returns a random quote
+ * Returns a random tip
  * @param {Client} client Discord server client
  * @param {Message} msg Command
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
-    let index = Math.floor(Math.random() * quotes.length);
+    let index = Math.floor(Math.random() * tips.length);
     const color = Math.floor(Math.random() * colors);
     const quote = new MessageEmbed()
         .setColor(color);
     if (args[1] && !isNaN(args[1])) {
         let idx = parseInt(args[1]) - 1;
-        if (idx < quotes.length && idx > 0) {
+        if (idx < tips.length && idx > 0) {
             index = idx;
         }
     }
-    quote.setDescription(`*${quotes[index].text}*`)
-        .setFooter(`- ${quotes[index].author}`)
-        .setTitle(`Random Quote #${index + 1}`);
+    quote.setDescription(`*${tips[index].text}*`)
+        .setFooter(`Try asking me for another tip!`)
+        .setTitle(`Random Tip #${index + 1}`);
     msg.channel.send(quote);
 }

1b49df9ec983c955b2fb90a05a23656c5f0d643f
Gideon Tong <gideon@gideontong.com>
2020-12-13 14:27:15 -0800

    Begin reaction command

--- commands/react.js
+++ commands/react.js
@@ -1,12 +1,12 @@
 const log = require('log4js').getLogger('amy');
 
 /**
- * Prints an emoji, regardless of whether it was animated or not
- * @param {Client} client Discord client
- * @param {Message} msg Command
+ * Reacts to the last message with an emote of choice (if possible)
+ * @param {Client} client Discord server client
+ * @param {Message} message Command
  * @param {Array} args Arguments
  */
-module.exports = async (client, msg, args) => {
+module.exports = async (client, message, args) => {
     var emoji;
     if (args.length < 2) {
         message.channel.send('You need to provide an emote!');
@@ -24,7 +24,11 @@ module.exports = async (client, msg, args) => {
         emoji = client.emojis.resolve(args[1]);
     }
     if (emoji) {
-        msg.channel.send(emoji.toString());
-        log.info(`${msg.author.tag} ${msg.author} told me to repeat :${emoji.name}: ${emoji.animated? '(animated)': ''}`);
+        message.delete();
+        let messages = message.channel.messages.cache;
+        message.channel.send(messages.keyArray());
+    } else {
+        message.channel.send("I couldn't find that emote...");
+        log.warn(`User tried to react with ${args[1]}`)
     }
 }

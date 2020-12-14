97edc9059896f4a279593ceaac3fcf7ac5ffd4f8
Gideon Tong <gideon@gideontong.com>
2020-12-13 17:36:25 -0800

    Add a new creepy command

---
+++ commands/creepy.js
@@ -0,0 +1,21 @@
+const subreddit = 'creepypms';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random creepy PM
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send('A quick tutorial on how to be creepy:')
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit battlestation picture I got ${err}`);
+    }
+}

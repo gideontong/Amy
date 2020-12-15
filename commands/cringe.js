dceac8f2aac95fac5ae9a5dc6d3bfce739146742
Gideon Tong <gideon@gideontong.com>
2020-12-14 18:52:22 -0800

    Add  cringe command

---
+++ commands/cringe.js
@@ -0,0 +1,20 @@
+const subreddit = 'cringepics';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random battlestation
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit cringe picture I got ${err}`);
+    }
+}

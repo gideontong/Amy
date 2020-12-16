cf77d1db1d5d67ffa408fe31bbf07815038a333a
Gideon Tong <gideon@gideontong.com>
2020-12-16 15:38:15 -0800

    Test paying money

--- commands/pay.js
+++ commands/pay.js
@@ -1,4 +1,7 @@
+const error = 'Send money to friends with `pay <user> <amount>`';
+
 const { transferBalance } = require('../lib/Member');
+const log = require('log4js').getLogger('amy');
 
 /**
  * Pay another user
@@ -7,4 +10,31 @@ const { transferBalance } = require('../lib/Member');
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
+    if (args.length != 3) {
+        msg.channel.send(error);
+        return;
+    }
+    const payor = msg.author.id;
+    let payee;
+    let amount;
+    try {
+        payee = msg.mentions.members.firstKey();
+        amount = parseInt(args[2]);
+    } catch (err) {
+        msg.channel.send(error);
+        return;
+    }
+    if (payor && payee && amount) {
+        const err = transferBalance(payor, payee, amount, false, function (data) {
+            msg.channel.send(`Successfully transferred $${amount} to <@${payee}>!`);
+            log.info(`${msg.author.tag} transferred ${amount} to ${msg.mentions.members.first().user.tag}`);
+        });
+        if (err) {
+            msg.channel.send(err);
+            return;
+        }
+    } else {
+        msg.channel.send(error);
+        return;
+    }
 }

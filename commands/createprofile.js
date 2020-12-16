0bfe21e504215d3a08f47176ff77c2e4d364645a
Gideon Tong <gideon@gideontong.com>
2020-12-15 22:13:25 -0800

    Add a createprofile command

---
+++ commands/createprofile.js
@@ -0,0 +1,12 @@
+const { createProfile } = require('../lib/Member');
+
+/**
+ * Creates a profile on the database for the user
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    createProfile(msg.author.id);
+    msg.channel.send(`Created profile with ${msg.author.id}`);
+}

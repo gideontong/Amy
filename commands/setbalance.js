4197ef4add01cdcb6d7eae555ec9fd8bfc4e82f3
Gideon Tong <gideon@gideontong.com>
2020-12-16 14:48:09 -0800

    Add a setbalance command

---
+++ commands/setbalance.js
@@ -0,0 +1,31 @@
+const { setBalance } = require('../lib/Member');
+const { extractSnowflake } = require('../lib/Validation');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Set a user balance
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    if (args.length != 3) {
+        msg.channel.send('Command usage: `setbalance <user> <amount>`');
+        return;
+    }
+    try {
+        const snowflake = extractSnowflake(args[1]);
+        const amount = parseInt(args[2]);
+        if (snowflake && amount) {
+            setBalance(snowflake, amount, function (data) {
+                msg.channel.send(`Successfully set <@${snowflake}>'s balance to $${data}!`);
+            });
+        } else {
+            msg.channel.send('Either snowflake or amount failed to parse. Check logs.');
+            log.error(`Failed to parse, got snowflake: ${snowflake} and amount: ${amount}`);
+        }
+    } catch (err) {
+        msg.channel.send('Something strange happened, check the logs.');
+        log.error(`Tried to set balance, got ${err}`);
+    }
+}

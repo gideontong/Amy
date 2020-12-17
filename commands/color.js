e4e5fe0a7b32f2760aa27bc031a19901c5b39cbc
Gideon Tong <gideon@gideontong.com>
2020-12-17 13:27:24 -0800

    Upgrade embed

--- commands/color.js
+++ commands/color.js
@@ -1,4 +1,5 @@
 const { colors } = require('../config/colortable.json');
+const { MessageEmbed } = require('discord.js');
 
 /**
  * Gets a color name given a color
@@ -11,8 +12,12 @@ module.exports = async (client, msg, args) => {
         msg.channel.send('Get a color by providing a Hex value! Usage: `color #000000`');
         return;
     }
-    color = getColor(args[1]);
-    msg.channel.send(color[1]);
+    const color = getColor(args[1]);
+    const embed = new MessageEmbed()
+        .setColor(color[0])
+        .setTitle(color[1])
+        .setThumbnail(`https://singlecolorimage.com/get/${color[0]}/400x400`);
+    msg.channel.send(embed);
 }
 
 /**
@@ -23,7 +28,7 @@ module.exports = async (client, msg, args) => {
 function getColor(color) {
     color = color.toUpperCase();
     if (color.length < 3 || color.length > 7)
-        return ["#000000", "Invalid Color: " + color, false];
+        return ["000000", "Invalid Color: " + color, false];
     if (color.length % 3 == 0)
         color = "#" + color;
     if (color.length == 4)
@@ -38,7 +43,7 @@ function getColor(color) {
 
     for (var i = 0; i < colors.length; i++) {
         if (color == "#" + colors[i][0])
-            return ["#" + colors[i][0], colors[i][1], true];
+            return [colors[i][0], colors[i][1], true];
 
         ndf1 = Math.pow(r - colors[i][2], 2) + Math.pow(g - colors[i][3], 2) + Math.pow(b - colors[i][4], 2);
         ndf2 = Math.pow(h - colors[i][5], 2) + Math.pow(s - colors[i][6], 2) + Math.pow(l - colors[i][7], 2);
@@ -46,11 +51,10 @@ function getColor(color) {
         if (df < 0 || df > ndf) {
             df = ndf;
             cl = i;
-            console.log(color, df, cl, colors[i], rgb, hsl);
         }
     }
 
-    return (cl < 0 ? ["#000000", "Invalid Color: " + color, false] : ["#" + colors[cl][0], colors[cl][1], false]);
+    return (cl < 0 ? ["000000", "Invalid Color: " + color, false] : [colors[cl][0], colors[cl][1], false]);
 }
 
 /**

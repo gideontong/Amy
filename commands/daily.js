5ae1ba4c43bb9b3bf138ce38bbd4ca06617c5d9b
Gideon Tong <gideon@gideontong.com>
2020-12-18 10:15:37 -0800

    Implement daily command

--- commands/daily.js
+++ commands/daily.js
@@ -1,3 +1,6 @@
+const { currency, passives } = require('../config/economy.json');
+const { checkCooldown, updateBalance } = require("../lib/Member");
+
 /**
  * Get your dailies!
  * @param {Client} client Discord server client
@@ -5,4 +8,15 @@
  * @param {Array} args Arguments
  */
 module.exports = async (client, msg, args) => {
+    let expiry = new Date();
+    expiry.setHours(expiry.getHours() + 24);
+    checkCooldown(msg.author.id, 'daily', function (err = null) {
+        if (err) {
+            msg.channel.send('You must wait 24 hours to get your next daily!');
+        } else {
+            updateBalance(msg.author.id, passives.daily, function (amount) {
+                msg.channel.send(`Congratulations, ${msg.member.nickname ? msg.member.nickname : msg.author.username}! You were granted ${currency}${passives.daily} and you now have ${currency}${amount} in your account.`);
+            });
+        }
+    }, expiry);
 }

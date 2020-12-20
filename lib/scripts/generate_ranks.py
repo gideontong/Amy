f9bf254d04452e02f2123db675dd580430b6245a
Gideon Tong <gideon@gideontong.com>
2020-12-19 19:27:42 -0800

    Fix for broken rank name

--- lib/scripts/generate_ranks.py
+++ lib/scripts/generate_ranks.py
@@ -2,7 +2,7 @@ from csv import DictReader as reader
 from json import dump
 
 data = {}
-with open('ranks.csv') as file, open('ranks.json', 'w') as out:
+with open('ranks.csv', encoding='utf-8') as file, open('ranks.json', 'w') as out:
     i_reader = reader(file)
     for row in i_reader:
         data[row['Rank']] = {

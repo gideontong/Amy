6dd4e3472d509d99ad221f10012c508de7a5e20c
Gideon Tong <gideon@gideontong.com>
2020-12-21 02:45:57 -0800

    Prepare polls rewrite

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,12 +1,7 @@
-// const { updateExistingPoll } = require('../lib/Survey');
 const { countAction } = require('../lib/Member');
 const log = require('log4js').getLogger('amy');
 
 module.exports = async (reaction, user) => {
-    // let run = reaction.me && reaction.message.editable && !user.bot;
-    // if (run) {
-    //     updateExistingPoll(reaction);
-    // }
     try {
         if (!user.bot) countAction(user.id, 'react');
     } catch (err) {
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,9 +1,2 @@
-// Dependencies
-const { updateExistingPoll } = require('../lib/Survey');
-
 module.exports = async (reaction, user) => {
-    let run = reaction.me && reaction.message.editable && !user.bot;
-    if (run) {
-        updateExistingPoll(reaction);
-    }
 }

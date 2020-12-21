5b0c45975c92902288a0661f684567ca7c1b86d0
Gideon Tong <gideon@gideontong.com>
2020-12-21 15:09:05 -0800

    Add more flavortext

--- commands/work.js
+++ commands/work.js
@@ -14,9 +14,10 @@ const log = require('log4js').getLogger('amy');
  */
 module.exports = async (client, msg, args) => {
     workJob(msg.author.id, msg.channel, function (award = 0, bonus = 0) {
-        if (award) {
-            let embed = new MessageEmbed()
-                .setColor(Math.floor(Math.random() * colors))
+        let embed = new MessageEmbed()
+            .setColor(Math.floor(Math.random() * colors))
+        if (award > 0) {
+            embed
                 .setDescription(`While working, you earned a total of ${currency}${award}!`)
                 .setTitle('👩‍🏭 Work Complete!')
             if (Math.random() < 0.2) {
@@ -27,8 +28,16 @@ module.exports = async (client, msg, args) => {
             if (bonus) {
                 embed.addField('Bonus Awarded!', `Your bosses even decided that you were such a hard worker, that part of your salary was a ${currency}${bonus} bonus!`);
             }
-            msg.channel.send(embed);
+        } else if (award < 0) {
+            embed
+                .setDescription(`You managed to lose money while working, causing you to spend ${currency}${Math.abs(award)}! Now that's unlucky.`)
+                .setTitle('Right career field?');
+        } else {
+            embed
+                .setDescription('Dang... your job doesn\'t pay very much? Sad.')
+                .setTitle('No pay this time!')
         }
+        msg.channel.send(embed);
         log.info(`${msg.author.tag} worked a job and earned ${award}`);
     });
 }

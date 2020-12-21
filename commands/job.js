90d51fe5534329df5797d4bfecb7f335213c3022
Gideon Tong <gideon@gideontong.com>
2020-12-21 00:23:31 -0800

    Add job function actually sets

--- commands/job.js
+++ commands/job.js
@@ -1,4 +1,6 @@
 const { isValidJob } = require("../lib/Economy");
+const { setJob } = require("../lib/Member");
+const log = require('log4js').getLogger('amy');
 
 /**
  * Sets your current job
@@ -15,8 +17,14 @@ module.exports = async (client, msg, args) => {
     const search = args.join('');
     const job = isValidJob(search);
     if (job) {
-        msg.channel.send('Your job has been set to ' + job)
-        log.info(`${msg.author.tag} set their job to ${job}`);
+        setJob(msg.author.id, job, function ([success, newJob, cooldown]) {
+            if (success) {
+                msg.reply(`Your new job is ${newJob}! Work begins immediately, so get crackin'!`);
+                log.info(`${msg.author.tag} set their job to ${newJob}`);
+            } else {
+                msg.channel.send('You can only change your job every 12 hours!');
+            }
+        });
     } else {
         msg.channel.send("I can't seem to find that job. Maybe try using `jobs` to see what jobs you qualify for?");
     }

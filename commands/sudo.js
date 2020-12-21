3ad6579586bc45386e89453cb03d33bf57a48458
Gideon Tong <gideon@gideontong.com>
2020-12-20 18:22:23 -0800

    Add sudo command back as easter egg

---
+++ commands/sudo.js
@@ -0,0 +1,13 @@
+/**
+ * Easter egg
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    if (msg.content.toLowerCase().includes('make me a sandwich')) {
+        msg.channel.send('https://imgs.xkcd.com/comics/sandwich.png');
+    } else {
+        msg.channel.send(`${msg.author.toString()} is not in the sudoers file. This incident will be reported.`);
+    }
+}

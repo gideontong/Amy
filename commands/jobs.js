cfb86ad245a4db86a035e9bf3a57a7fd0d6afa0f
Gideon Tong <gideon@gideontong.com>
2020-12-20 19:51:59 -0800

    Best test jobs command

--- commands/jobs.js
+++ commands/jobs.js
@@ -1,2 +1,11 @@
+const { getJobs } = require("../lib/Economy")
+
+/**
+ * See available jobs to you
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
 module.exports = async (client, msg, args) => {
+    msg.channel.send(getJobs(parseInt(args[1])));
 }

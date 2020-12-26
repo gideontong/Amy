a0f23dffd873b1f71ee0b802b66fcccd12bcc419
Gideon Tong <gideon@gideontong.com>
2020-12-25 20:25:39 -0800

    Fix embed typo

--- commands/trivia.js
+++ commands/trivia.js
@@ -16,15 +16,17 @@ const colors = [
  * @param {Message} msg Command
  * @param {Array} args Arguments
  */
-module.exports = async(client, msg, args) => {
+module.exports = async (client, msg, args) => {
     let color = 0;
-    const embed =  {
-        title: "Welcome to Amy's Trivia Game!",
-        description: "This game supports both singleplayer and multiplayer. The more people that play, the bigger the prize pool. Compete to win large prizes!\n\nDo you want to play __singleplayer__ or __multiplayer__?\n:bust_in_silhouette: Singleplayer\n:busts_in_silhouette: Multiplayer",
-        color: color % colors.length,
-        footer: {
-          text: `You have ${timeout} seconds to answer.`
+    const embed = {
+        embed: {
+            title: "Welcome to Amy's Trivia Game!",
+            description: "This game supports both singleplayer and multiplayer. The more people that play, the bigger the prize pool. Compete to win large prizes!\n\nDo you want to play __singleplayer__ or __multiplayer__?\n:bust_in_silhouette: Singleplayer\n:busts_in_silhouette: Multiplayer",
+            color: color % colors.length,
+            footer: {
+                text: `You have ${timeout} seconds to answer.`
+            }
         }
-      };
+    };
     msg.channel.send(embed);
 }

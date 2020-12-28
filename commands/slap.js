b6eeff89caf25d90bde9024e4c4a8e1d7ca307d0
Gideon Tong <gideon@gideontong.com>
2020-12-28 13:22:00 -0800

    Add slap command

---
+++ commands/slap.js
@@ -0,0 +1,31 @@
+const pleading = '<:pleading:792119409551867925>';
+
+/**
+ * Slap someone
+ * @param {Client} client Discord server client
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (client, msg, args) => {
+    if (msg.mentions.users.size > 0) {
+        const snowflake = msg.mentions.users.firstKey();
+        if (snowflake == client.user.id) {
+            msg.reply(`Why are you trying to slap me? ${pleading} ${pleading}`);
+            return;
+        }
+        msg.channel.send(`<@${msg.author.id}> 🤚💥 slapped <@${snowflake}> across the face! React with 🤚 in 5 seconds to retaliate!`)
+            .then(message => {
+                message.react('🤚');
+                const filter = (reaction, user) => {
+                    return reaction.emoji.name == '🤚' && user.id == snowflake;
+                }
+                message.awaitReactions(filter, { max: 1, time: 5000, errors: ['time'] })
+                    .then(collected => {
+                        msg.channel.send(`<@${snowflake}> 🤚💥 punched <@${msg.author.id}> back!`);
+                    })
+                    .catch(collected => { });
+            });
+    } else {
+        msg.channel.send('You... want to punch yourself?');
+    }
+}

9c8507d2b562a53e0ac0a19ea95bffe0be23c938
Gideon Tong <gideon@gideontong.com>
2021-01-11 15:47:19 -0800

    Fix NaN error

--- commands/pinnedstats.js
+++ commands/pinnedstats.js
@@ -9,7 +9,7 @@ module.exports = async (msg, args) => {
     pinned.forEach(message => {
         const author = message.author;
         const current = counter.get(author);
-        counter.set(author, current ? 1 : current + 1);
+        counter.set(author, Number.isNaN(current) ? 1 : current + 1);
     });
     const description = generateLeaderboard(counter);
     const embed = {

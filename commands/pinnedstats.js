222aabedee68b8c518f0d683a78b6a85e991420f
Gideon Tong <gideon@gideontong.com>
2021-01-11 15:44:32 -0800

    Remove sorted

--- commands/pinnedstats.js
+++ commands/pinnedstats.js
@@ -26,35 +26,10 @@ module.exports = async (msg, args) => {
  * @param {Map} counter Counts of people
  */
 function generateLeaderboard(counter) {
-    // Sort if size is less than 10 due to inefficient algorithm
-    if (counter.size < 10) {
-        return generateSortedLeaderboard(counter);
-    } else {
-        const warning = 'More than 10 people on the leaderboard is currently unsupported, so it may look weird.\n\n';
-        let description = new String();
-        counter.forEach(count, user => {
-            description += `${user}: ${count}`;
-        });
-        return warning + description;
-    }
-}
-
-/**
- * Generate sorted leaderboard
- * @param {Map} counter Counts of people
- */
-function generateSortedLeaderboard(counter) {
+    const warning = 'More than 10 people on the leaderboard is currently unsupported, so it may look weird.\n\n';
     let description = new String();
-    while (counter.size > 0) {
-        let high = -1, last = null;
-        for (const [user, count] of counter.entries()) {
-            if (count > high) {
-                high = count;
-                last = user;
-            }
-        }
-        description += `${last}: ${high}\n`;
-        counter.delete(last);
-    }
-    return description;
+    counter.forEach(count, user => {
+        description += `${user}: ${count}`;
+    });
+    return warning + description;
 }

0ebdf3de891437e7a878130caa05a4fd2c3c5edf
Gideon Tong <gideon@gideontong.com>
2021-01-10 20:27:10 -0800

    Add Algeria command

---
+++ commands/algeria.js
@@ -0,0 +1,19 @@
+const subreddit = 'algeriapics';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/AlgeriaPics
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/AlgeriaPics picture I got ${err}`);
+    }
+}

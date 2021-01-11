c4122b5dcdcd97ed45a43fd47df6309ef67a2dd5
Gideon Tong <gideon@gideontong.com>
2021-01-10 20:19:59 -0800

    Add North Korea command

---
+++ commands/northkorea.js
@@ -0,0 +1,19 @@
+const subreddit = 'nothkoreapics';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/NothKoreaPics
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/NorthKoreaPics picture I got ${err}`);
+    }
+}

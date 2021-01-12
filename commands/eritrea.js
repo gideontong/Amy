361b26c560217ce03eff5f4858614438a446e6f7
Gideon Tong <gideon@gideontong.com>
2021-01-11 18:12:40 -0800

    Add Eritrea command

---
+++ commands/eritrea.js
@@ -0,0 +1,19 @@
+const subreddit = 'eritreapics';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/EritreaPics
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/EritreaPics picture I got ${err}`);
+    }
+}

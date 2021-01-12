4d1d92c3c5607822b210de36a15f920572f51444
Gideon Tong <gideon@gideontong.com>
2021-01-11 18:40:00 -0800

    Separate commands into folders

--- commands/debugslots.js
+++
@@ -1,9 +0,0 @@
-/**
- * Debugging slots command
- * @param {Message} msg Command
- * @param {Array} args Command arguments
- */
-module.exports = async (msg, args) => {
-    msg.channel.send('Starting slots in DEBUG mode. Your balance will not be affected.');
-    require('./slots')(msg, args);
-}
--- commands/vote.js
+++
@@ -1,7 +0,0 @@
-/**
- * Get lootcrates
- * @param {Message} msg Message
- * @param {Array} args Arguments
- */
-module.exports = async (msg, args) => {
-}

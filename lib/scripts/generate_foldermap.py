f2b49f7f64ae29d0cbae3cc347fbb6dbc7c5d903
Gideon Tong <gideon@gideontong.com>
2021-01-11 19:10:37 -0800

    Add foldermap generator

---
+++ lib/scripts/generate_foldermap.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+from os import listdir
+from json import dump
+
+head = '../../commands/'
+folders = listdir(head)
+commands = dict()
+
+def parse_commands(folder, name):
+    files = [name.split('.')[0] for name in listdir(folder)]
+    for file in files:
+        commands[file] = name
+
+for folder in folders:
+    parse_commands(head + folder, folder)
+
+with open('../../config/foldermap.json', 'w') as file:
+    dump(commands, file, sort_keys=True)

ba6762097468e6ccc50cf784ecae39048c5f1163
Gideon Tong <gideon@gideontong.com>
2021-01-11 18:14:28 -0800

    Add Ethiopia command

---
+++ commands/ethiopia.js
@@ -0,0 +1,19 @@
+const subreddit = 'ethiopiapics';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/EthiopiaPics
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/EthiopiaPics picture I got ${err}`);
+    }
+}

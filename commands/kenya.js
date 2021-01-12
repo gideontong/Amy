13a82f9651bcd7289996dc3c0311bb2e012b843c
Gideon Tong <gideon@gideontong.com>
2021-01-11 18:21:13 -0800

    Add Kenya command

---
+++ commands/kenya.js
@@ -0,0 +1,19 @@
+const subreddit = 'kenyapics';
+
+const { getRedditImage } = require('../lib/Internet');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/KenyaPics
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    try {
+        getRedditImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/KenyaPics picture I got ${err}`);
+    }
+}

dbaeb7ffd7caf65383efb334d539d954e2b724ef
Gideon Tong <gideon@gideontong.com>
2021-01-14 03:17:12 -0800

    Remove cooldown from yearly command

--- commands/economy/yearly.js
+++ commands/economy/yearly.js
@@ -1,5 +1,5 @@
 const { currency, passives } = require('../../config/economy.json');
-const { checkCooldown, updateBalance } = require('../../lib/Member');
+const { updateBalance } = require('../../lib/Member');
 
 /**
  * You're really desperate, aren't you?
@@ -7,15 +7,7 @@ const { checkCooldown, updateBalance } = require('../../lib/Member');
  * @param {Array} args Arguments
  */
 module.exports = async (msg, args) => {
-    let expiry = new Date();
-    expiry.setFullYear(expiry.getFullYear() + 1);
-    checkCooldown(msg.author.id, 'yearly', function (err = null) {
-        if (err) {
-            msg.channel.send('You must wait a year to get your next yearly!');
-        } else {
-            updateBalance(msg.author.id, passives.yearly, function (amount) {
-                msg.channel.send(`Congratulations, ${msg.member.nickname ? msg.member.nickname : msg.author.username}! You were granted ${currency}${passives.yearly} and you now have ${currency}${amount} in your account.`);
-            });
-        }
-    }, expiry);
+    updateBalance(msg.author.id, passives.yearly, function (amount) {
+        msg.channel.send(`Congratulations, ${msg.member.nickname ? msg.member.nickname : msg.author.username}! You were granted ${currency}${passives.yearly} and you now have ${currency}${amount} in your account.`);
+    });
 }

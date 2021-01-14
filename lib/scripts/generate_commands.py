adaaf7d6fdd27187ae1f52e0671750f4dc07c455
Gideon Tong <gideon@gideontong.com>
2021-01-13 18:19:13 -0800

    Change command generator format

--- lib/scripts/generate_commands.py
+++ lib/scripts/generate_commands.py
@@ -1,21 +1,60 @@
 #!/bin/python3
 
 """
-Usage: python3 convert_commands.py input output
+Usage: python3 generate_commands.py input output
 
-input JSON file
-output CSV file
-
-Converts commands to CSV
+input file JSON or CSV
+output file JSON or CSV but not same as input
 """
 
-from json import load
-from csv import writer
+from json import load, dump
+from csv import DictReader as reader, writer
 import sys
 
+if len(sys.argv) != 3:
+    print('You need to provide an input and an output.')
+    exit()
+
+in_file = sys.argv[1]
+out_file = sys.argv[2]
+
+def to_JSON():
+    data = {}
+    with open(in_file, encoding='utf-8') as csv_file, open(out_file, 'w', encoding='utf-8') as json_file:
+        csv_reader = reader(csv_file)
+        for command in csv_reader:
+            data[command['Command']] = {
+                'command': command['Command'],
+                'name': command['Name'],
+                'description': command['Description'],
+                'category': command['Category'],
+                'alias': command['Aliases'].split(','),
+                'flags': {
+                    'admin': command['Admin'] == 'TRUE',
+                    'premium': command['Premium'] == 'TRUE',
+                    'hidden': command['Secret'] == 'TRUE',
+                    'nsfw': command['NSFW'] == 'TRUE'
+                },
+                'cooldown': {
+                    'standard': float(command['Cooldown']),
+                    'premium': float(command['PaidCooldown'])
+                }
+            }
+        dump(data, json_file)
+
+def to_CSV():
+    pass
+
+if in_file.endswith('json'):
+    to_CSV()
+elif in_file.endswith('csv'):
+    to_JSON()
+
+"""
 with open(sys.argv[1], encoding='utf-8') as json_file, open(sys.argv[2], 'w', encoding='utf-8') as csv_file:
     commands = load(json_file)
     out = writer(csv_file)
     for command in commands['commands']:
         arr = [command['name'], command['command'], command['description'], ','.join(command['alias'])]
         out.writerow(arr)
+"""

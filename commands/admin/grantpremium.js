bc5861a673ac8065d8065d5f38409c7144e79d34
Gideon Tong <gideon@gideontong.com>
2021-01-14 20:54:17 -0800

    Complete grantpremium command

--- commands/admin/grantpremium.js
+++ commands/admin/grantpremium.js
@@ -1,7 +1,44 @@
+const allowed = [
+    'days',
+    'weeks',
+    'months',
+    'years'
+];
+
+const { setPremium } = require('../../lib/Member');
+
 /**
  * Grants premium
  * @param {Message} msg Command
  * @param {Array} args Arguments
  */
 module.exports = async (msg, args) => {
+    if (args.length != 4 ||
+        msg.mentions.users.size != 1 ||
+        isNaN(args[2]) ||
+        allowed.includes(args[3])) {
+        msg.channel.send('Grant premium status with `grantpremium <user> <length> <days|weeks|months|years>');
+    }
+    const snowflake = msg.mentions.users.firstKey();
+    let days = parseInt(args[2]);
+    switch (args[3]) {
+        case 'weeks':
+            days *= 7;
+            break;
+        case 'months':
+            days *= 30;
+            break;
+        case 'years':
+            days *= 365;
+            break;
+        default:
+            break;
+    }
+    setPremium(function (premium, expiry) {
+        if (premium) {
+            msg.channel.send(`Granted premium! <@${snowflake}>'s premium now expires on ${expiry.toDateString()}.`);
+        } else {
+            msg.channel.send('Something went wrong and was unable to give premium.')
+        }
+    }, snowflake, days);
 }

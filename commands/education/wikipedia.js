7aa996940b7ddc04fc8a59a67372c08c900231b5
Gideon Tong <gideon@gideontong.com>
2021-02-02 01:51:21 -0800

    Add Wikipedia embeds

--- commands/education/wikipedia.js
+++ commands/education/wikipedia.js
@@ -11,7 +11,33 @@ module.exports = async (msg, args) => {
     const query = args.join(' ');
     pageSearch(function (title, text) {
         if (text.length > 0) {
+            const embed = {
+                title: title,
+                description: text,
+                url: `https://en.wikipedia.org/wiki/${title}`,
+                color: 9624918,
+                author: {
+                    name: 'Wikipedia (English)',
+                    icon_url: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png'
+                },
+                footer: {
+                    text: 'Powered by Amy. Closest match found.'
+                }
+            };
+            return channel.send({ embed: embed });
         } else {
+            const embed = {
+                description: "I searched far and wide, but I couldn't find a similar match or a suitable match to the thing you were searching. You might want to try again?",
+                color: 16711680,
+                author: {
+                    name: 'Wikipedia Has Failed!',
+                    icon_url: "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png"
+                },
+                footer: {
+                    text: 'Powered by Amy. Try a new search?'
+                }
+            };
+            return channel.send({ embed: embed });
         }
     }, query);
 }

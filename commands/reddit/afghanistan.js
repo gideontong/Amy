42212c3bd9f8fa82aa11b27b534a758d206296b4
Gideon Tong <gideon@gideontong.com>
2021-02-04 17:21:57 -0800

    Add Afghanistan and Antartica commands

---
+++ commands/reddit/afghanistan.js
@@ -0,0 +1,19 @@
+const subreddit = 'afghanistanpics';
+
+const { getRandomImage } = require('../../lib/Reddit');
+const log = require('log4js').getLogger('amy');
+
+/**
+ * Returns a random photo from /r/AfghanistanPics
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    try {
+        getRandomImage(function (data = 'Loading...') {
+            msg.channel.send(data);
+        }, subreddit);
+    } catch (err) {
+        log.error(`While trying to grab a Reddit /r/AfghanistanPics picture I got ${err}`);
+    }
+}

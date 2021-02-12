b8d04955d8fd9caecb8769d82b6077d2fc322b49
Gideon Tong <gideon@gideontong.com>
2021-02-12 14:06:00 -0800

    Add EZ clap command

---
+++ commands/fun/ezclap.js
@@ -0,0 +1,11 @@
+const emotes = '<:EZ:809906880062685265> <a:clap:809906878225055744>';
+
+/**
+ * EZ clap
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    const channel = msg.channel;
+    channel.send(emotes);
+}

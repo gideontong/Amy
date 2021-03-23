8bfb043d79af278a1ce475818aa075eff5bb9658
Gideon Tong <gideon@gideontong.com>
2021-03-23 09:50:23 -0700

    A fix for the futile command

--- commands/fun/futile.js
+++ commands/fun/futile.js
@@ -1,10 +1,8 @@
-const { sankeys } = require('../../config/fun.json');
-
 /**
  * Yes, life is futile
  * @param {Message} msg Command
  * @param {Array} args Arguments
  */
-module.exports = async(client, msg, args) => {
-    msg.channel.send(sankeys[Math.floor(Math.random() * sankeys.length)]);
+module.exports = async(msg, args) => {
+    msg.channel.send('Life is truly futile...');
 }

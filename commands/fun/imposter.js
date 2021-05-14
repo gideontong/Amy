9caeea885d82dea9c0fcbb68ba35a0e8e5073d47
Gideon Tong <gideon@gideontong.com>
2021-05-14 14:11:54 -0700

    Prepare imposter command

---
+++ commands/fun/imposter.js
@@ -0,0 +1,11 @@
+// Coming soon
+
+const use = `. 　　　。　　　　•　 　ﾟ　　。 　　.
+　　　.　　　 　　.　　　　　。　　 。　. 　
+.　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
+　　ﾟ  <someone> was An Impostor.　 。　.
+　　'　　　 0 Impostor remains 　 　　。
+　　ﾟ　　　.　　　. ,　　　　.　 .`
+
+module.exports = async (msg, args) => {
+}

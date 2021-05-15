18ff712847d9d3786761287dc31d87f5e6733c80
Gideon Tong <gideon@gideontong.com>
2021-05-15 01:17:37 -0700

    Add imposter command

--- commands/fun/imposter.js
+++ commands/fun/imposter.js
@@ -1,11 +1,21 @@
-// Coming soon
-
-const use = `. 　　　。　　　　•　 　ﾟ　　。 　　.
-　　　.　　　 　　.　　　　　。　　 。　. 　
-.　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
-　　ﾟ  <someone> was An Impostor.　 。　.
-　　'　　　 0 Impostor remains 　 　　。
-　　ﾟ　　　.　　　. ,　　　　.　 .`
-
+/**
+ * Is there an imposter?
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
 module.exports = async (msg, args) => {
+    const channel = msg.channel;
+    const members = channel.members;
+    const member = members.randomKey();
+    
+    var imposters = members.size > 3 ? 2 : 0;
+    const messages = [
+        '```. 　　　。　　　　•　 　ﾟ　　。 　　.',
+        '　　　.　　　 　　.　　　　　。　　 。　. 　',
+        '.　　 。　　　　　 ඞ 。 . 　　 • 　　　　•',
+        `　　ﾟ  <@${member}> was an impostor.　 。　.`,
+        `　　'　　　 ${imposters} Impostors Remain 　 　　。`,
+        '　　ﾟ　　　.　　　. ,　　　　.　 .```'
+    ];
+    channel.send(messages.join('\n'));
 }

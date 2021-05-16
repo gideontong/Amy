18dcab0ace0377b65471dcc9c4fe4e3423781f4a
Gideon Tong <gideon@gideontong.com>
2021-05-16 16:46:11 -0700

    Remove small feature

--- app.py
+++
@@ -1,9 +0,0 @@
-from sys import version_info
-
-MANDATORY_VERSION = (3, 7)
-assert version_info >= MANDATORY_VERSION
-
-from server import webserver
-
-if __name__ == '__main__':
-    webserver.run(host='0.0.0.0', port=7075)

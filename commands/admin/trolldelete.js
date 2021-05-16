e883c80df4d1a8df95e398cdcce45276ad4f34bd
Gideon Tong <gideon@gideontong.com>
2021-05-15 21:18:40 -0700

    Add a trolldelete command

---
+++ commands/admin/trolldelete.js
@@ -0,0 +1,31 @@
+const TO_DELETE = 20;
+const MESSAGES = [
+    'Did you know Leo is gay?',
+    'Every time you go live, you can go live.',
+    'Sasha Grey is an American actress according to Wikipedia.',
+    'This is an unmoderated channel.',
+    'Sleeping on the job again, are we?',
+    '640K ought to be enough for anybody.',
+    'This process is automatic.'
+];
+
+/**
+ * Send and delete a bunch of messages
+ * @param {Message} msg Command
+ * @param {Array} args Arguments
+ */
+module.exports = async (msg, args) => {
+    const channel = msg.channel;
+
+    msg.delete();
+    for (let i = 0; i < TO_DELETE; i++) {
+        sendAndDelete(channel);
+    }
+}
+
+function sendAndDelete(channel) {
+    channel.send(MESSAGES[Math.floor(Math.random() * MESSAGES.length)])
+        .then(msg => {
+            msg.delete();
+        });
+}

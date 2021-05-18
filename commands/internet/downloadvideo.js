ac78e75ea74b8fbf32d13369608d1cc0efcbff3d
Gideon Tong <gideon@gideontong.com>
2021-05-18 00:43:40 -0700

    Temporary patch for downloadvideo command

--- commands/internet/downloadvideo.js
+++ commands/internet/downloadvideo.js
@@ -8,47 +8,60 @@ const { getInfo } = require('youtube-dl');
  * @param {Array} args Command arguments
  */
 module.exports = async (msg, args) => {
+    const channel = msg.channel;
+
     if (args.length != 2) {
-        msg.channel.send(`To download a video, use ${prefix.amy}downloadvideo [URL]`);
+        return channel.send({
+            embed: {
+                description: `To download a video, use ${prefix.amy}downloadvideo [URL]`
+            }
+        });
     }
+
     let url = args[1];
     try {
         new URL(url);
     } catch (_) {
-        msg.channel.send('Are you sent me a link to a video?');
-        log.warn(`Tried to download ${url}, failed.`)
-        return;
+        return channel.send({
+            embed: {
+                title: 'Was that a URL?',
+                description: 'Please make sure you provide a valid URL to download!'
+            }
+        });
     }
+
     getInfo(url, [], function (err, data) {
         if (err) {
-            msg.channel.send('Something went wrong, call Gideon for help.');
             log.error(`${msg.author.tag} ${msg.author} ran ${msg.content} that threw ${err}`);
-            return;
+            return channel.send({
+                embed: {
+                    title: 'Error downloading video!',
+                    description: 'Something went wrong, try joining the support server to report this error. Does your video exist?'
+                }
+            });
         }
 
         let description = data.description.length > 1000 ? data.description.substring(0, 1000) + '...' : data.description;
         let descLines = description.split(/\r?\n/);
         if (descLines.length > 5) {
             description = descLines[0];
-            for(var i = 1; i < 5; i++) {
+            for (var i = 1; i < 5; i++) {
                 description += '\n' + descLines[i];
             }
         }
 
         let embed = {
-            "embed": {
-                "title": data.title,
-                "description": `Your download is ready! Click [here](${data.url}) to download your video.\n\n**About your video:**\n${description}...`,
-                "url": args[1],
-                "author": {
-                    "name": "Amy's Video Downloader"
-                },
-                "thumbnail": {
-                    "url": data.thumbnail
-                }
+            title: data.title,
+            description: `Your download is ready! Click [here](${data.url}) to download your video.\n\n**About your video:**\n${description}...`,
+            url: args[1],
+            author: {
+                name: "Amy's Video Downloader"
+            },
+            thumbnail: {
+                url: data.thumbnail
             }
         };
-        msg.channel.send(embed);
         log.info(`${msg.author.tag} ${msg.author} downloaded video ${url} and got ${data.title}`);
+        return channel.send({ embed: embed });
     })
 }

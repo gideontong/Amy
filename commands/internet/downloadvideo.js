b9a8d041475bc46695240c6f5562964cfdf3568c
Gideon Tong <gideon@gideontong.com>
2021-05-18 01:01:46 -0700

    Fix video downloader

--- commands/internet/downloadvideo.js
+++ commands/internet/downloadvideo.js
@@ -1,6 +1,6 @@
 const { prefix } = require('../../config/config.json');
 const log = require('log4js').getLogger('amy');
-const { getInfo } = require('youtube-dl');
+const downloader = require('youtube-dl-exec');
 
 /**
  * Downloads an online video and provides a download link
@@ -30,8 +30,46 @@ module.exports = async (msg, args) => {
         });
     }
 
-    getInfo(url, [], function (err, data) {
-        if (err) {
+    downloader(url, {
+        dumpSingleJson: true,
+        noWarnings: true,
+        noCallHome: true,
+        youtubeSkipDashManifest: true
+    })
+        .then(data => {
+            if (!data.formats) {
+                return channel.send({
+                    embed: {
+                        title: 'No downloads available...',
+                        description: 'We found your video, but couldn\'t find a download link. Sorry!'
+                    }
+                });
+            }
+
+            let description = data.description.length > 1000 ? data.description.substring(0, 1000) + '...' : data.description;
+            let descLines = description.split(/\r?\n/);
+            if (descLines.length > 5) {
+                description = descLines[0];
+                for (var i = 1; i < 5; i++) {
+                    description += '\n' + descLines[i];
+                }
+            }
+
+            let embed = {
+                title: data.title,
+                description: `Your download is ready! Click [here](${data.formats[0].url}) to download your video.\n\n**About your video:**\n${description}...`,
+                url: args[1],
+                author: {
+                    name: "Amy's Video Downloader"
+                },
+                thumbnail: {
+                    url: data.thumbnail
+                }
+            };
+            log.info(`${msg.author.tag} ${msg.author} downloaded video ${url} and got ${data.title}`);
+            return channel.send({ embed: embed });
+        })
+        .catch(err => {
             log.error(`${msg.author.tag} ${msg.author} ran ${msg.content} that threw ${err}`);
             return channel.send({
                 embed: {
@@ -39,29 +77,5 @@ module.exports = async (msg, args) => {
                     description: 'Something went wrong, try joining the support server to report this error. Does your video exist?'
                 }
             });
-        }
-
-        let description = data.description.length > 1000 ? data.description.substring(0, 1000) + '...' : data.description;
-        let descLines = description.split(/\r?\n/);
-        if (descLines.length > 5) {
-            description = descLines[0];
-            for (var i = 1; i < 5; i++) {
-                description += '\n' + descLines[i];
-            }
-        }
-
-        let embed = {
-            title: data.title,
-            description: `Your download is ready! Click [here](${data.url}) to download your video.\n\n**About your video:**\n${description}...`,
-            url: args[1],
-            author: {
-                name: "Amy's Video Downloader"
-            },
-            thumbnail: {
-                url: data.thumbnail
-            }
-        };
-        log.info(`${msg.author.tag} ${msg.author} downloaded video ${url} and got ${data.title}`);
-        return channel.send({ embed: embed });
-    })
+        });
 }

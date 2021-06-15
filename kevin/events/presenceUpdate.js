b3f7b1c61169843df3ed950ce5ac89077f35736f
Gideon Tong <gideon@gideontong.com>
2021-06-15 15:05:24 -0700

    Update presence

--- kevin/events/presenceUpdate.js
+++ kevin/events/presenceUpdate.js
@@ -14,6 +14,14 @@ const trackingGuild = '691848461217169438';
 const updateChannel = '735272343773118504';
 const log = require('log4js').getLogger('kevin');
 
+// TODO:
+// User has woken up
+// User has gone back to sleep
+// User has finally turned on their computer
+// Leo has gone outside (main on mobile)
+// Leo is taking a dump (second account online)
+// User is wanking (posting in sauce channel)
+
 /**
  * Determine a presence change. Used for updating when
  * someone wakes up and such.
@@ -21,23 +29,36 @@ const log = require('log4js').getLogger('kevin');
  * @param {Presence} newPresence New Presence data
  */
 module.exports = async (oldPresence, newPresence) => {
+    if (!oldPresence) return;
+
     const guild = newPresence.guild;
     if (!(guild.available && guild.id == trackingGuild)) return;
 
     const snowflake = newPresence.userID;
     if (!(snowflake in enabledUsers)) return;
     const name = enabledUsers[snowflake];
-    
-    log.info(`${name} changed their status to ${newPresence.status}`);
 
     const client = newPresence.client;
     const channels = client.channels;
     
     channels.fetch(updateChannel)
         .then((channel) => {
+            const emit = new Array();
+            
             if (oldPresence.status != newPresence.status) {
-                channel.send(`${name} changed their status from ${oldPresence.status} to ${newPresence.status}`);
+                emit.push(`from ${oldPresence.status} to ${newPresence.status}`);
             }
+
+            const oldStatus = oldPresence.clientStatus;
+            const newStatus = newPresence.clientStatus;
+            const oldMobile = 'mobile' in oldStatus && oldStatus['mobile'] != 'idle';
+            const newMobile = 'mobile' in newStatus && newStatus['mobile'] != 'idle';
+            if (oldMobile != newMobile) {
+                emit.push('mobile status');
+            }
+
+            channel.send(name + ' changed ' + emit.join(' and '))
+                .catch(_ => { });
         })
-        .catch(err => { });
+        .catch(_ => { });
 }
